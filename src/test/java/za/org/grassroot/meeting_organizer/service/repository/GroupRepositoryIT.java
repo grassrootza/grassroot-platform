201409d718d7a0269c691f305bec3f40d8b216b4
luke-grassroot <luke.jordan@grassroot.org.za>
2015-07-23 00:06:04 +0200

    Added Lombok, and Many-to-Many relatinoships
    As a minor adjustmet, added Lombok to the Maven dependencies and
    ticked off the to-do on using it for the hashcode, equals and tostring
    implementations. Also tried to wire up the many-to-many relatinoship
    among groups and users, and wrote an extra test on the group CRUD
    repository for it. Unfortunately, causes tests to break, to do with
    tables in test database seemingly not being cleared, not clear why.

--- src/test/java/za/org/grassroot/meeting_organizer/service/repository/GroupRepositoryIT.java
+++ src/test/java/za/org/grassroot/meeting_organizer/service/repository/GroupRepositoryIT.java
@@ -14,6 +14,11 @@ import za.org.grassroot.meeting_organizer.DbUnitConfig;
 import za.org.grassroot.meeting_organizer.model.Group;
 import za.org.grassroot.meeting_organizer.model.User;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
@@ -53,6 +58,37 @@ public class GroupRepositoryIT {
         assertNotNull(groupFromDb.getCreatedDateTime());
         assertThat(groupFromDb.getGroupName(), is("TestGroup"));
         assertThat(groupFromDb.getCreatedByUser().getPhoneNumber(), is("56789"));
+    }
+
+    @Test
+    public void shouldSaveAndRetrieveGroupMembers() throws Exception {
+        assertThat(groupRepository.count(), is(0L));
+
+        Group groupToCreate = new Group();
+
+        User firstUserToCreate = new User();
+        firstUserToCreate.setPhoneNumber("12345");
+        User secondUserToCreate = new User();
+        secondUserToCreate.setPhoneNumber("67890");
+
+        List<User> userList = Arrays.asList(firstUserToCreate, secondUserToCreate); // CRUD repository requires iterable
+        userRepository.save(userList);
+
+        groupToCreate.setGroupName("TestGroup");
+        groupToCreate.setCreatedByUser(firstUserToCreate);
+        groupToCreate.setGroupMembers(userList);
+        assertNull(groupToCreate.getId());
+        assertNull(groupToCreate.getCreatedDateTime());
+        groupRepository.save(groupToCreate);
+
+        assertThat(groupRepository.count(), is(1L));
+        Group groupFromDb = groupRepository.findAll().iterator().next();
+        assertNotNull(groupFromDb.getId());
+        assertNotNull(groupFromDb.getCreatedDateTime());
+
+        List<User> membersFromDb = groupFromDb.getGroupMembers();
+        assertThat(membersFromDb.get(0).getPhoneNumber(), is("12345"));
+        assertThat(membersFromDb.get(1).getPhoneNumber(), is("67890"));
     }
 
 }
