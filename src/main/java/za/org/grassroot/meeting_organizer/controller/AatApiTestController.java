8ced96f40b59693b59fa2b87aac5c1f8a59498ee
luke-grassroot <luke.jordan@grassroot.org.za>
2015-07-24 08:18:25 +0200

    Started building out the USSD menus
    Created the "home page" for the USSD system, with a few options, and
    created a USSD version of a 404, for stuff we're still working on.

--- src/main/java/za/org/grassroot/meeting_organizer/controller/AatApiTestController.java
+++ src/main/java/za/org/grassroot/meeting_organizer/controller/AatApiTestController.java
@@ -2,6 +2,8 @@ package za.org.grassroot.meeting_organizer.controller;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 
 import org.springframework.http.MediaType;
@@ -15,16 +17,40 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 /**
  * Controller to play around with the AAT api
+ * To do: abstract the tuples of menu option and URL redirect
+ * To do: abstract out the messages, so can introduce a dictionary mechanism of some sort to deal with languages
  */
 @RequestMapping(method = GET, produces = MediaType.APPLICATION_XML_VALUE)
 @RestController
 public class AatApiTestController {
 
-    @RequestMapping(value = "/question1")
+    String baseURI = "http://meeting-organizer.herokuapp.com/ussd/";
+
+    @RequestMapping(value = "/ussd/test_question")
     @ResponseBody
     public Request question1() throws URISyntaxException {
         final Option option = new Option("Yes I can!", 1,1, new URI("http://yourdomain.tld/ussdxml.ashx?file=2"),true);
         return new Request("Can you answer the question?", Collections.singletonList(option));
     }
+
+    @RequestMapping(value = "/ussd/start")
+    @ResponseBody
+    public Request startMenu() throws URISyntaxException {
+        String welcomeMessage = "Hello! Welcome to GrassRoot. What do you want to do?";
+        final Option meetingOrg = new Option("Call a meeting", 1,1, new URI(baseURI + "mtg"),true);
+        final Option voteTake = new Option("Take a vote", 2,2, new URI(baseURI + "vote"),true);
+        final Option logAction = new Option("Record an action", 3,3, new URI(baseURI + "log"),true);
+        final Option userProfile = new Option("Change my profile", 4, 4, new URI(baseURI + "user"), true);
+        final Option manageGroups = new Option("Manage groups", 5, 5, new URI(baseURI + "group"), true);
+        return new Request(welcomeMessage, Arrays.asList(meetingOrg, voteTake, logAction, userProfile, manageGroups));
+    }
+
+    @RequestMapping(value = { "/ussd/error", "/ussd/mtg", "/ussd/vote", "/ussd/log", "/ussd/user", "ussd/group" })
+    @ResponseBody
+    public Request notBuilt() throws URISyntaxException {
+        String errorMessage = "Sorry! We haven't built that yet. We're working on it.";
+        return new Request(errorMessage, new ArrayList<Option>());
+    }
+
 
 }
