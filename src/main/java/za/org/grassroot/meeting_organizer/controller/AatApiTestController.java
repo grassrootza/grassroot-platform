306c26f434f191dabac3fe1806109429ef3b2515
luke-grassroot <luke.jordan@grassroot.org.za>
2015-07-24 14:46:13 +0200

    Linked up to the repository, and trying out free text entry
    Added in a check on the first page if user exists, and, if not, auto
    store them in the repository. Also added checks in subsequent menus
    to pick up latent errors. Made the second menu in the meeting organizer
    a free text entry and then added the check to see what we get back.

--- src/main/java/za/org/grassroot/meeting_organizer/controller/AatApiTestController.java
+++ src/main/java/za/org/grassroot/meeting_organizer/controller/AatApiTestController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import za.org.grassroot.meeting_organizer.model.AAT.Option;
 import za.org.grassroot.meeting_organizer.model.AAT.Request;
+import za.org.grassroot.meeting_organizer.model.User;
+import za.org.grassroot.meeting_organizer.service.repository.UserRepository;
+
+import javax.print.URIException;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
@@ -21,12 +26,16 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
  * To do: abstract the tuples of menu option and URL redirect
  * To do: abstract out the messages, so can introduce a dictionary mechanism of some sort to deal with languages
  * To do: write a phone number parsing method / converter so we get them consistent. Coming in as 27....
+ * To do: avoid hard-coding the URLs in the menus, so we can swap them around later
  */
 @RequestMapping(method = GET, produces = MediaType.APPLICATION_XML_VALUE)
 @RestController
 public class AatApiTestController {
 
     String baseURI = "http://meeting-organizer.herokuapp.com/ussd/";
+
+    @Autowired
+    UserRepository userRepository;
 
     @RequestMapping(value = "/ussd/test_question")
     @ResponseBody
@@ -37,9 +46,20 @@ public class AatApiTestController {
 
     @RequestMapping(value = "/ussd/start")
     @ResponseBody
-    public Request startMenu(@RequestParam(value="msisdn") String phoneNumber) throws URISyntaxException {
+    public Request startMenu(@RequestParam(value="msisdn") String passedNumber) throws URISyntaxException {
         // So we need to create a user from this. Here we go.
-        String welcomeMessage = "Hello! Welcome to GrassRoot. Your number is " + phoneNumber + ". What do you want to do?";
+        String phoneNumber = convertPhoneNumber(passedNumber);
+
+        // todo: probably another auxiliary function here ("loadOrSaveUser")
+        User sessionUser = new User();
+        if (userRepository.findByPhoneNumber(phoneNumber).isEmpty()) {
+            sessionUser.setPhoneNumber(phoneNumber);
+            userRepository.save(sessionUser);
+        } else {
+            sessionUser = userRepository.findByPhoneNumber(phoneNumber).iterator().next();
+        }
+
+        String welcomeMessage = "Hello! Welcome to GrassRoot. What do you want to do?";
         final Option meetingOrg = new Option("Call a meeting", 1,1, new URI(baseURI + "mtg"),true);
         final Option voteTake = new Option("Take a vote", 2,2, new URI(baseURI + "vote"),true);
         final Option logAction = new Option("Record an action", 3,3, new URI(baseURI + "log"),true);
@@ -50,17 +70,40 @@ public class AatApiTestController {
 
     @RequestMapping(value = "/ussd/mtg")
     @ResponseBody
-    public Request meetingOrg(@RequestParam(value="msisdn", required=true) String phoneNumber) {
-        // We'll use the phone number to persist the user.
-        String returnMessage = "Okay, we'll set up a meeting.";
-        return new Request(returnMessage, new ArrayList<Option>());
+    public Request meetingOrg(@RequestParam(value="msisdn", required=true) String inputNumber) throws URISyntaxException {
+        String phoneNumber = convertPhoneNumber(inputNumber);
+        if (userRepository.findByPhoneNumber(phoneNumber).isEmpty()) {
+            return new Request("Error! This shouldn't happen.", new ArrayList<Option>());
+        } else {
+            // note: not calling the instance of User as we don't use anything ... yet
+            // todo: look up the user's groups, and give a menu of those, if they exist (w/ scrolling)
+            // todo: Add an example in promptMessage, but make sure not too long (already too much, maybe).
+            String promptMessage = "Okay, we'll set up a meeting. Please enter the numbers of the people to invite.";
+            Option freeText = new Option("", 1, 1, new URI(baseURI + "mtg2"), true);
+            return new Request(promptMessage, Collections.singletonList(freeText));
+        }
     }
 
-    @RequestMapping(value = { "/ussd/error", "/ussd/vote", "/ussd/log", "/ussd/user", "ussd/group" })
+    @RequestMapping(value = { "/ussd/error", "ussd/mtg2", "/ussd/vote", "/ussd/log", "/ussd/user", "ussd/group" })
     @ResponseBody
     public Request notBuilt() throws URISyntaxException {
         String errorMessage = "Sorry! We haven't built that yet. We're working on it.";
         return new Request(errorMessage, new ArrayList<Option>());
+    }
+
+    // todo: decide on our preferred string format, for now keeping it at for 27 (not discarding info)
+    // todo: add error handling to this.
+    // todo: consider using Guava libraries, or another, for when we get to tricky user input
+    // todo: put this in a wrapper class for a bunch of auxiliary methods? think we'll use this a lot
+    public String convertPhoneNumber(String inputString) {
+        int codeLocation = inputString.indexOf("27");
+        boolean hasCountryCode = (codeLocation >= 0 && codeLocation < 2); // allowing '1' for '+' and 2 for '00'
+        if (hasCountryCode) {
+            return inputString.substring(codeLocation);
+        } else {
+            String truncedNumber = (inputString.charAt(0) == '0') ? inputString.substring(1) : inputString;
+            return "27" + truncedNumber;
+        }
     }
 
 
