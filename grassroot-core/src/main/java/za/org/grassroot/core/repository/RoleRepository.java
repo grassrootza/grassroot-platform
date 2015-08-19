3b2bcc7792790c284b72e44c393957259cd72422
aakil786 <muhammad786>
2015-08-19 23:26:40 +0200

    changes to group and user and grouprepository, functionality incomplete and will probably be refactored

--- grassroot-core/src/main/java/za/org/grassroot/core/repository/RoleRepository.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/repository/RoleRepository.java
@@ -2,13 +2,9 @@ package za.org.grassroot.core.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import za.org.grassroot.core.domain.Role;
-import za.org.grassroot.core.domain.User;
-
-import java.util.List;
 
 /**
  * @author Lesetse Kimwaga
  */
 public interface RoleRepository extends CrudRepository<Role,Long> {
-
 }
