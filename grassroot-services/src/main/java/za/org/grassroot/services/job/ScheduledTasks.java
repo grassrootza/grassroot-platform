7e2cf3d7f5f169b08588b4892107cdb77203d37b
aakil786 <muhammad786>
2015-10-07 22:03:03 +0200

    scheduled task, not complete just so that final commit is easier

---
+++ grassroot-services/src/main/java/za/org/grassroot/services/job/ScheduledTasks.java
@@ -0,0 +1,26 @@
+package za.org.grassroot.services.job;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Logger;
+
+/**
+ * Created by aakilomar on 10/5/15.
+ */
+
+@Component
+public class ScheduledTasks {
+
+    private Logger log = Logger.getLogger(getClass().getCanonicalName());
+
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 60000)
+    public void reportCurrentTime() {
+        log.info("The time is now " + dateFormat.format(new Date()));
+    }
+}
