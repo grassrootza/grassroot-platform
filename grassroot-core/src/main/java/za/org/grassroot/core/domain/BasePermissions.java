2a4e7ee246289bc255b3bac61549a56525fb5558
lesetse <lesetse@anylytical.co.za>
2015-10-08 09:26:00 +0200

    Added bases roles and permissions and scafolding for change scripts

---
+++ grassroot-core/src/main/java/za/org/grassroot/core/domain/BasePermissions.java
@@ -0,0 +1,14 @@
+package za.org.grassroot.core.domain;
+
+/**
+ * @author Lesetse Kimwaga
+ */
+public final class BasePermissions {
+
+    public static final String GROUP_EDIT_GROUP_DETAILS        = "GROUP_EDIT_GROUP_DETAILS";
+    public static final String GROUP_CREATE_MEMBER_INVITATIONS = "GROUP_CREATE_MEMBER_INVITATION";
+    public static final String GROUP_REMOVE_MEMBER             = "GROUP_REMOVE_MEMBER";
+    public static final String GROUP_VIEW_MEMBER_DETAILS       = "GROUP_VIEW_MEMBER_DETAILS";
+    public static final String GROUP_CREATE_MEETING_EVENTS     = "GROUP_CREATE_MEETING_EVENTS";
+    public static final String GROUP_EDIT_MEETING_EVENTS       = "GROUP_EDIT_MEETING_EVENTS";
+}
---
+++ grassroot-core/src/main/java/za/org/grassroot/core/domain/BaseRoles.java
@@ -0,0 +1,16 @@
+package za.org.grassroot.core.domain;
+
+/**
+ * @author Lesetse Kimwaga
+ */
+public final class BaseRoles {
+
+    public static final String ADMINISTRATOR   = "ADMINISTRATOR";
+    public static final String USER            = "USER";
+    public static final String GROUP_OWNER     = "GROUP_OWNER";
+    public static final String GROUP_MANAGER   = "GROUP_MANAGER";
+    public static final String GROUP_MEMBER    = "GROUP_MEMBER";
+    public static final String GROUP_MODERATOR = "GROUP_MODERATOR";
+
+
+}
