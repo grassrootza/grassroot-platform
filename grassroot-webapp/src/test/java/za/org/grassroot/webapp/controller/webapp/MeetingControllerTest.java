e3eff05398615cde4ba58238aa4ba4182859d75f
Paballo Ditshego <pdditshego@gmail.com>
2016-01-08 16:56:42 +0200

    MeetingControllerTest, some test are commented  out, until i have read some more literature on testing.

--- grassroot-webapp/src/test/java/za/org/grassroot/webapp/controller/webapp/MeetingControllerTest.java
+++ grassroot-webapp/src/test/java/za/org/grassroot/webapp/controller/webapp/MeetingControllerTest.java
@@ -3,47 +3,23 @@ package za.org.grassroot.webapp.controller.webapp;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.powermock.api.mockito.PowerMockito;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.SpringApplicationContextLoader;
-import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.security.web.FilterChainProxy;
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.*;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.*;
-
-
-
-import static org.mockito.Mockito.*;
-import static org.hamcrest.Matchers.hasProperty;
-import static org.hamcrest.Matchers.*;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import javax.annotation.Resource;
-
+import za.org.grassroot.core.domain.Event;
 import za.org.grassroot.core.domain.Group;
 import za.org.grassroot.core.domain.User;
-import za.org.grassroot.services.EventManagementService;
-import za.org.grassroot.services.GroupManagementService;
-import za.org.grassroot.services.UserManagementService;
-import za.org.grassroot.webapp.Application;
+import za.org.grassroot.core.enums.EventRSVPResponse;
 
-import org.junit.runner.RunWith;
-import org.springframework.security.web.debug.DebugFilter;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 
@@ -52,171 +28,260 @@ import org.springframework.security.web.debug.DebugFilter;
  */
 
 
-
-
 public class MeetingControllerTest extends WebAppAbstractUnitTest {
 
-	private static final Logger logger= LoggerFactory.getLogger(MeetingControllerTest.class);
+    private static final Logger logger = LoggerFactory.getLogger(MeetingControllerTest.class);
 
-	
-    @Mock
-    protected UserManagementService userManagementServiceMock;
 
-    @Mock
-    protected GroupManagementService groupManagementServiceMock;
+    @InjectMocks
+    private MeetingController meetingController;
 
-    @Mock
-    protected EventManagementService eventManagementServiceMock;
 
-	
-	@Autowired 
-	private FilterChainProxy springSecurityFilterChain; 
-	
-	@Autowired
-	protected WebApplicationContext webAppContext;
-	
-	@InjectMocks
-	private MeetingController meetingController;
+    @Before
+    public void setUp() {
+        setUp(meetingController);
 
-	
-	
 
-	@Before
-	public void setUp()  {
-		MockitoAnnotations.initMocks(this);
-		setUp(meetingController);
-		
-	
-	
+    }
 
-	}
 
-	/*
-	 * @Test public void shouldShowMeetingDetails() throws Exception {
-	 * 
-	 * // will have to figure out a way to do it cleanly long eventId = 1L;
-	 * Event dummyMeeting = new Event(); dummyMeeting.setId(eventId);
-	 * 
-	 * User dummyUser = new User("", "testUser"); dummyUser.setId(1L);
-	 * List<User> listOfDummyYesResponses = mock(List.class); Map<User,
-	 * EventRSVPResponse> dummyRsvpResponses = mock(Map.class);
-	 * 
-	 * when(eventManagementServiceMock.loadEvent(eventId)).thenReturn(
-	 * dummyMeeting);
-	 * when(eventManagementServiceMock.getListOfUsersThatRSVPYesForEvent(
-	 * dummyMeeting)) .thenReturn(listOfDummyYesResponses);
-	 * when(eventManagementServiceMock.getRSVPResponses(dummyMeeting)).
-	 * thenReturn(dummyRsvpResponses);
-	 * when(listOfDummyYesResponses.size()).thenReturn(1);
-	 * 
-	 * mockMvc.perform(get("/meeting/view").param("eventId",
-	 * String.valueOf(eventId))).andExpect(status().isOk())
-	 * .andExpect(view().name("meeting/view"))
-	 * .andExpect(model().attribute("meeting", hasProperty("id", is(1L))))
-	 * .andExpect(model().attribute("rsvpYesTotal", equalTo(1))); //
-	 * .andExpect(model().attribute("rsvpResponses",hasEntry("dummyUser",
-	 * EventRSVPResponse.YES)));
-	 * 
-	 * verify(eventManagementServiceMock, times(1)).loadEvent(eventId); //
-	 * verify(eventManagementServiceMock, //
-	 * times(1)).getListOfUsersThatRSVPYesForEvent(dummyMeeting); //
-	 * verify(eventManagementServiceMock, //
-	 * times(1)).getRSVPResponses(dummyMeeting);
-	 * 
-	 * }
-	 * 
-	 * /*
-	 * 
-	 * @Test public void TestCreateMeetingWorks() throws Exception{
-	 * 
-	 * Event dummyMeeting = new Event();
-	 * 
-	 * long selectedGroupId = 1L; long time = System.currentTimeMillis();
-	 * 
-	 * dummyMeeting.setEventStartDateTime(new Timestamp(time));
-	 * dummyMeeting.setDateTimeString(new SimpleDateFormat("E d MMM HH:mm")
-	 * .format(dummyMeeting.getEventStartDateTime())); dummyMeeting.setId(1L);
-	 * 
-	 * 
-	 * when(eventManagementServiceMock.updateEvent(dummyMeeting)).thenReturn(
-	 * dummyMeeting); when(eventManagementServiceMock.setGroup(dummyMeeting,
-	 * selectedGroupId)).thenReturn(dummyMeeting);
-	 * 
-	 * mockMvc.perform(post("/meeting/create").param("selectedGroupId",
-	 * String.valueOf(selectedGroupId))).andExpect(status().isOk())
-	 * .andExpect(view().name("redirect:/home"))
-	 * .andExpect(redirectedUrl("/home"));
-	 * 
-	 * 
-	 * 
-	 * }
-	 */
+    @Test
+    public void shouldShowMeetingDetails() throws Exception {
 
-	@Test
-	public void TestSendFreeFormWorksWithGroupSpecified() throws Exception {
+        long eventId = 1L;
+        Event dummyMeeting = new Event();
+        dummyMeeting.setId(eventId);
 
-		Long groupId = 1L;
-		
+        User dummyUser = new User("", "testUser");
+        dummyUser.setId(1L);
+        List<User> listOfDummyYesResponses = mock(List.class);
 
-		User testUser = new User("", "testuser");
-		Group testGroup = new Group("", testUser);
-		testGroup.setId(groupId);		
-		
-		
-		
-		when(groupManagementServiceMock.loadGroup(groupId)).thenReturn(testGroup);
-		
+        HashMap<User, EventRSVPResponse> dummyResponsesMap = mock(HashMap.class);
+        dummyResponsesMap.put(dummyUser, EventRSVPResponse.INVALID_RESPONSE.YES);
 
-		mockMvc.perform(get("/meeting/free").with(user("")).param("groupId", String.valueOf(groupId)))
-				.andExpect(view().name("meeting/free"))
-				.andExpect(model().attribute("group", hasProperty("id", is(1L))));
-		// .andExpect(model().attribute("groupSpecified", true));
+        Set<Map.Entry<User, EventRSVPResponse>> dummyRsvpResponses =
+                dummyResponsesMap.entrySet();
 
-		verify(groupManagementServiceMock, times(1)).loadGroup(groupId);
-		verifyNoMoreInteractions(groupManagementServiceMock);
 
-	}
-	
-	
-}
+        when(eventManagementServiceMock.loadEvent(eventId)).thenReturn(
+                dummyMeeting);
+        when(eventManagementServiceMock.getListOfUsersThatRSVPYesForEvent(
+                dummyMeeting)).thenReturn(listOfDummyYesResponses);
+        when(eventManagementServiceMock.getRSVPResponses(dummyMeeting)).
+                thenReturn(dummyResponsesMap);
 
+        when(listOfDummyYesResponses.size()).thenReturn(1);
+        when(dummyResponsesMap.entrySet()).thenReturn(dummyRsvpResponses);
+
+        mockMvc.perform(get("/meeting/view").param("eventId",
+                String.valueOf(eventId))).andExpect(status().isOk())
+                .andExpect(view().name("meeting/view"))
+                .andExpect(model().attribute("meeting", hasProperty("id", is(1L))))
+                .andExpect(model().attribute("rsvpYesTotal", equalTo(1)));
+
+
+        verify(eventManagementServiceMock, times(1)).loadEvent(eventId); //
+        verify(eventManagementServiceMock, //
+                times(1)).getListOfUsersThatRSVPYesForEvent(dummyMeeting); //
+        verify(eventManagementServiceMock, //
+                times(1)).getRSVPResponses(dummyMeeting);
+
+    }
+
+
+   /* @Test
+    public void TestCreateMeetingWorks() throws Exception {
+
+        Event dummyMeeting = new Event();
+
+        long selectedGroupId = 1L;
+
+        dummyMeeting.setId(1L);
+
+
+        when(eventManagementServiceMock.updateEvent(dummyMeeting)).thenReturn(
+                dummyMeeting);
+        when(eventManagementServiceMock.setGroup(dummyMeeting,
+                selectedGroupId)).thenReturn(dummyMeeting);
+
+        mockMvc.perform(post("/meeting/create").param("selectedGroupId",
+                String.valueOf(selectedGroupId))).andExpect(status().isOk())
+                .andExpect(view().name("redirect:/home"))
+                .andExpect(redirectedUrl("/home"));
+
+
+    }*/
+
+    @Test
+    public void TestSendFreeFormWorksWithGroupSpecified() throws Exception {
+
+
+        Long groupId = 1L;
+        User testUser = new User("", "testuser");
+        Group testGroup = new Group("", testUser);
+        testGroup.setId(groupId);
+
+        when(groupManagementServiceMock.loadGroup(groupId)).thenReturn(testGroup);
+
+        mockMvc.perform(get("/meeting/free").with(user(testUserPhone)).param("groupId", String.valueOf(groupId)))
+                .andExpect(status().isOk())
+                .andExpect(view().name("meeting/free"))
+                .andExpect(model().attribute("group", hasProperty("id", is(groupId))))
+                .andExpect(model().attribute("groupSpecified", is(true)));
+
+        verify(groupManagementServiceMock, times(1)).loadGroup(groupId);
+        verifyNoMoreInteractions(groupManagementServiceMock);
+
+    }
+
+
+    @Test
+
+    public void sendFreeFormWorksWithGroupNotSpecified() throws Exception {
+
+
+        User testUser = new User("", "testUser");
+        ArrayList<Group> dummyGroups = new ArrayList<>();
+        Group dummyGroup = new Group("", testUser);
+        dummyGroup.setId(1L);
+        dummyGroups.add(dummyGroup);
+
+
+        when(groupManagementServiceMock.getGroupsPartOf(testUser)).thenReturn(
+                dummyGroups);
+        when(userManagementServiceMock.fetchUserByUsername(testUserPhone)).thenReturn(testUser);
+
+        mockMvc.perform(get("/meeting/free").with(user(testUserPhone))).andExpect((view().name("meeting/free"))
+        ).andExpect(model().attribute("userGroups", hasItems(dummyGroup)))
+                .andExpect(model().attribute("groupSpecified", is(false)));
+
+        verify(groupManagementServiceMock, times(1)).getGroupsPartOf(testUser);
+        verifyNoMoreInteractions(groupManagementServiceMock);
+
+    }
+
+
+    @Test
+    public void createMeetingIndexWorksWithGroupSpecified() throws Exception {
+
+
+        long id = 1L;
+
+
+        User testUser = new User("", "testUser");
+        Group testGroup = new Group("", testUser);
+        testGroup.setId(id);
+        testUser.setId(id);
+
+        Event dummyMeeting = new Event();
+        dummyMeeting.setId(id);
+        List<String[]> minuteOptions = mock(List.class);
+
+        when(userManagementServiceMock.fetchUserByUsername(testUserPhone)).thenReturn(testUser);
+        when(eventManagementServiceMock.createMeeting(testUser)).thenReturn(dummyMeeting);
+        when(groupManagementServiceMock.loadGroup(id)).thenReturn(testGroup);
+        when(eventManagementServiceMock.setGroup(dummyMeeting.getId(), id)).thenReturn(dummyMeeting);
+        when(eventManagementServiceMock.setEventNoReminder(dummyMeeting.getId())).thenReturn(dummyMeeting);
+
+        mockMvc.perform(get("/meeting/create").with(user(testUser)).param("groupId", String.valueOf(id)))
+                .andExpect((view().name("meeting/create")))
+                .andExpect(model().attribute("group", hasProperty("id", is(id)))).andExpect(model().attribute("meeting",
+                hasProperty("id", is(id)))).andExpect(model().attribute("groupSpecified", is(true)));
+        //  .andExpect(model().attribute("reminderOptions", hasItem(minuteOptions)));
+
+        verify(userManagementServiceMock, times(1)).fetchUserByUsername(testUserPhone);
+        verify(eventManagementServiceMock, times(1)).createMeeting(testUser);
+        verify(groupManagementServiceMock, times(1)).loadGroup(id);
+        verify(eventManagementServiceMock, times(1)).setGroup(dummyMeeting.getId(), id);
+        verify(eventManagementServiceMock, times(1)).setEventNoReminder(dummyMeeting.getId());
+        verifyNoMoreInteractions(userManagementServiceMock);
+        verifyNoMoreInteractions(eventManagementServiceMock);
+        verifyNoMoreInteractions(groupManagementServiceMock);
+
+
+    }
+
+    @Test
+    public void createMeetingIndexWorksWithGroupNotSpecified() throws Exception {
+
+        User testUser = new User("", "testUser");
+        testUser.setId(1L);
+
+        List<Group> dummyGroups = Arrays.asList(
+                new Group("", testUser), new Group("", testUser),
+                new Group("", testUser));
+       // dummyGroups.add(testGroup);
+        Event dummyMeeting = new Event();
+        dummyMeeting.setId(1L);
+        List<String[]> minuteOptions = mock(List.class);
+
+        when(userManagementServiceMock.fetchUserByUsername(testUserPhone)).thenReturn(testUser);
+        when(eventManagementServiceMock.createMeeting(testUser)).thenReturn(dummyMeeting);
+        when(eventManagementServiceMock.setEventNoReminder(dummyMeeting.getId())).thenReturn(dummyMeeting);
+        when(groupManagementServiceMock.getGroupsPartOf(testUser)).thenReturn(
+                dummyGroups);
+
+        mockMvc.perform(get("/meeting/create").with(user(testUserPhone))).andExpect((view().name("meeting/create"))
+        ).andExpect(model().attribute("groupSpecified", is(false)))
+              //  .andExpect(model().attribute("userGroups", hasItems(dummyGroups)))
+                .andExpect(model().attribute("meeting", hasProperty("id", is (1L))));
+
+        verify(userManagementServiceMock, times(1)).fetchUserByUsername(testUserPhone);
+        verify(eventManagementServiceMock,times(1)).createMeeting(testUser);
+        verify(eventManagementServiceMock, times (1)).setEventNoReminder(dummyMeeting.getId());
+        verify(groupManagementServiceMock, times(1)).getGroupsPartOf(testUser);
+
+
+
+
+    }
+
+     @Test
+    public void sendFreeMsgWorks() throws Exception{
+
+         Long id =1L;
+         Group dummyGroup = new Group();
+         dummyGroup.setId(id);
+         Event testEvent = new Event();
+         testEvent.setId(id);
+         User testUser = new User("","testUser");
+         boolean includeSubGroup = true;
+         String message ="message";
+
+
+         when(userManagementServiceMock.fetchUserByUsername(testUserPhone)).thenReturn(testUser);
+         when(groupManagementServiceMock.loadGroup(id)).thenReturn(dummyGroup);
+         when(eventManagementServiceMock.createEvent("",testUser,dummyGroup,includeSubGroup)).thenReturn(testEvent);
+         when(eventManagementServiceMock.sendManualReminder(testEvent,message)).thenReturn(true);
+
+
+
+
+
+
+
+
+
+     }
 
 
 /*
- * @Test
- * 
- * public void TestSendFreeFormWorksWithGroupNotSpecified() throws Exception {
- * 
- * // Boolean groupSpecified = mock(Boolean.class);
- * 
- * User testUser = new User("", "testUser"); List<Group> dummyGroups =
- * mock(ArrayList.class);
- * 
- * when(groupManagementServiceMock.getGroupsPartOf(testUser)).thenReturn(
- * dummyGroups); // when(groupSpecified).thenReturn(false);
- * 
- * mockMvc.perform(get("/meeting/free")).andExpect((view().name("/meeting/fre"))
- * ) .andExpect(model().attribute("userGroups", dummyGroups)); //
- * .andExpect(model().attribute("groupSpecified", false));
- * 
- * verify(groupManagementServiceMock, times(1)).getGroupsPartOf(testUser);
- * verifyNoMoreInteractions(groupManagementServiceMock);
- * 
- * }
- * 
- * @Test public void rsvpNoShouldWork() throws Exception {
- * 
- * }
- * 
- * @Test public void rsvpYesShouldWork() throws Exception {
- * 
- * }
- * 
- * @Test public void sendRemiderShoudWork() {
- * 
- * }
- * 
- * }
- */
+   @Test
+    public void rsvpNoShouldWork() throws Exception {
+
+    }
+
+    @Test
+    public void rsvpYesShouldWork() throws Exception {
+
+    }
+
+    @Test
+    public void sendRemiderShoudWork() {
+
+    }
+*/
+}
+
 
 // }
--- grassroot-webapp/src/test/java/za/org/grassroot/webapp/controller/webapp/WebAppAbstractUnitTest.java
+++ grassroot-webapp/src/test/java/za/org/grassroot/webapp/controller/webapp/WebAppAbstractUnitTest.java
@@ -1,40 +1,18 @@
 package za.org.grassroot.webapp.controller.webapp;
 
-import java.util.Properties;
-
-import org.apache.catalina.security.SecurityConfig;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.MessageSource;
-import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.context.WebApplicationContext;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.handler.SimpleMappingExceptionResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.spring4.expression.Mvc;
-
-import za.org.grassroot.services.EventLogManagementService;
-import za.org.grassroot.services.EventManagementService;
-import za.org.grassroot.services.GroupManagementService;
-import za.org.grassroot.services.LogBookService;
-import za.org.grassroot.services.UserManagementService;
-import za.org.grassroot.webapp.Application;
-import za.org.grassroot.webapp.MVCConfig;
+import za.org.grassroot.services.*;
 import za.org.grassroot.webapp.controller.BaseController;
-
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.SpringApplicationContextLoader;
 
 
 /*
@@ -42,13 +20,11 @@ import org.springframework.boot.test.SpringApplicationContextLoader;
  */
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@SpringApplicationConfiguration(classes = Application.class)
+@RunWith(MockitoJUnitRunner.class)
+@ContextConfiguration
 public abstract class WebAppAbstractUnitTest {
 	
 	protected MockMvc mockMvc;
-
 
 
     @Mock
@@ -56,24 +32,40 @@ public abstract class WebAppAbstractUnitTest {
     
     @Mock
     protected LogBookService logBookServiceMock;
-    
-    @Autowired
-    protected WebApplicationContext webApplicationContext;
-   
-    
-    
-    
-    
-    protected void setUp(){
-    	mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-    	
-    }
+
+
+    @Mock
+    protected UserManagementService userManagementServiceMock;
+
+    @Mock
+    protected GroupManagementService groupManagementServiceMock;
+
+    @Mock
+    protected EventManagementService eventManagementServiceMock;
+
+
+    protected final static String testUserPhone = "27815550000";
+
     
     
     protected void setUp(BaseController baseController){
+
+        mockAuthentication();
     	mockMvc = MockMvcBuilders.standaloneSetup(baseController).build();
     	
     }
+
+    protected void mockAuthentication(){
+
+
+         UsernamePasswordAuthenticationToken authentication =
+                new UsernamePasswordAuthenticationToken(testUserPhone, "12345");
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+    }
+
+
     
     
    
