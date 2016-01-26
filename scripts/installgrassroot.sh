da6eb414353d2b515d256a803b507276d4623577
Vincent <vinceotlo@gmail.com>
2016-01-26 08:15:08 +0000

    Initial appspec and related files.

---
+++ scripts/installgrassroot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#yum -y install httpd > /var/log/installapache.out 2>&1
+mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install
+
---
+++ scripts/startgrassroot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#service apache2 start > /var/log/startapache.out 2>&1
+#daemon  -F /var/grassroot/grass_root_pid `cat /etc/grassroot` java -Dspring.profiles.active=localpg -jar ./grassroot-webapp/target/grassroot-webapp-1.0-SNAPSHOT.jar --server.ssl.keyStore=keystore.jks
+. /etc/grassroot
+nohup java -XX:+UseCompressedOops -Dspring.profiles.active=heroku -jar ./grassroot-webapp/target/grassroot-webapp-1.0-SNAPSHOT.jar --server.ssl.keyStore=keystore.jks > /var/grassroot/grassroot-app.log 2>&1 &
+echo $! > /var/grassroot/.pid
