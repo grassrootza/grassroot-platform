a85555fc812633f59453819c3d44dc3fb6947d60
Vincent <vinceotlo@gmail.com>
2016-01-26 13:48:40 +0000

    :( :(

--- scripts/installgrassroot.sh
+++ scripts/installgrassroot.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 #yum -y install httpd > /var/log/installapache.out 2>&1
 . /etc/environment
-chown -R ubuntu:ubuntu *
-cd /var/grassroot && mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install 
+mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install 
