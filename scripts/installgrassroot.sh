943a3486f2cb06f5a86c0b84987004b78785e83a
Vincent <vinceotlo@gmail.com>
2016-01-26 09:05:38 +0000

    location fix

--- scripts/installgrassroot.sh
+++ scripts/installgrassroot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #yum -y install httpd > /var/log/installapache.out 2>&1
 CURR=$PWD;
-cd /var/grassroot/grassroot-platform && mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install && cd $CURR
+cd /var/grassroot && mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install && cd $CURR
