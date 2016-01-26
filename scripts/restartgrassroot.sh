96e009e20af2e293178158cbbd1a0d396608a461
Vincent <vinceotlo@gmail.com>
2016-01-26 08:46:34 +0000

    Simply copied start to restart

--- scripts/restartgrassroot.sh
+++ scripts/restartgrassroot.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-#service apache2 restart > /var/log/restartapache.out 2>&1
-. $PWD/startgrassroot.sh
+#service apache2 start > /var/log/startapache.out 2>&1
+#daemon  -F /var/grassroot/grass_root_pid `cat /etc/grassroot` java -Dspring.profiles.active=localpg -jar ./grassroot-webapp/target/grassroot-webapp-1.0-SNAPSHOT.jar --server.ssl.keyStore=keystore.jks
+. /etc/grassroot
+CURR=$PWD
+cd ..
+if [[ -f /var/grassroot/.pid ]]; then
+	kill -s SIGINT `cat /var/grassroot/.pid`;
+	rm -f  /var/grassroot/.pid;
+fi
+
+nohup java -XX:+UseCompressedOops -Dspring.profiles.active=heroku -jar ./grassroot-webapp/target/grassroot-webapp-1.0-SNAPSHOT.jar --server.ssl.keyStore=keystore.jks > /var/grassroot/grassroot-app.log 2>&1 &
+echo $! > /var/grassroot/.pid
+cd $CURR
