096bfcfd50b1e3583f997b2d5004cc41d2206d2d
Vincent <vinceotlo@gmail.com>
2016-01-26 08:59:02 +0000

    force location for compile (for now)

--- scripts/installgrassroot.sh
+++ scripts/installgrassroot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #yum -y install httpd > /var/log/installapache.out 2>&1
 CURR=$PWD;
-cd ..
+cd /var/grassroot/grassroot-platform 
 mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install
 cd $CURR
