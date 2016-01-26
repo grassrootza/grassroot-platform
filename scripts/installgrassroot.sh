204b547087166fc2042cae98379ca2dfb8459796
Vincent <vinceotlo@gmail.com>
2016-01-26 08:37:03 +0000

    directory reference before exec

--- scripts/installgrassroot.sh
+++ scripts/installgrassroot.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 #yum -y install httpd > /var/log/installapache.out 2>&1
+CURR=$PWD;
+cd ..
 mvn -Dmaven.test.skip=true -e clean compile && mvn -Dmaven.test.skip=true install
-
+cd $CURR
--- scripts/startgrassroot.sh
+++ scripts/startgrassroot.sh
@@ -2,5 +2,8 @@
 #service apache2 start > /var/log/startapache.out 2>&1
 #daemon  -F /var/grassroot/grass_root_pid `cat /etc/grassroot` java -Dspring.profiles.active=localpg -jar ./grassroot-webapp/target/grassroot-webapp-1.0-SNAPSHOT.jar --server.ssl.keyStore=keystore.jks
 . /etc/grassroot
+CURR=$PWD
+cd ..
 nohup java -XX:+UseCompressedOops -Dspring.profiles.active=heroku -jar ./grassroot-webapp/target/grassroot-webapp-1.0-SNAPSHOT.jar --server.ssl.keyStore=keystore.jks > /var/grassroot/grassroot-app.log 2>&1 &
 echo $! > /var/grassroot/.pid
+cd $CURR
