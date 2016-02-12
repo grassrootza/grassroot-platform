e72320fa85054ce26e835582321897ca347d078d
Vincent <vinceotlo@gmail.com>
2016-02-12 10:53:54 +0000

    Added graceful stop

--- scripts/stopgrassroot.sh
+++ scripts/stopgrassroot.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 if [[ -f /var/grassroot/.pid ]]; then
-        kill  `cat /var/grassroot/.pid`;
+        kill -TERM `cat /var/grassroot/.pid`;
+	mypid = `cat /var/grassroot/.pid`;
+	while [[ `ps -p $mypid > /dev/null;echo $?` -eq '1' ]]; do 
+		echo -n '.'; 
+		sleep 1; 
+	done
         rm -f  /var/grassroot/.pid;
 fi
 
