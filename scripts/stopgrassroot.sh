27ba4a521b37aa503c8c16b9f50bf92106b34b77
Vincent <vinceotlo@gmail.com>
2016-02-12 10:56:27 +0000

    spacing

--- scripts/stopgrassroot.sh
+++ scripts/stopgrassroot.sh
@@ -2,7 +2,7 @@
 
 if [[ -f /var/grassroot/.pid ]]; then
         kill -TERM `cat /var/grassroot/.pid`;
-	mypid = `cat /var/grassroot/.pid`;
+	mypid=`cat /var/grassroot/.pid`;
 	while [[ `ps -p $mypid > /dev/null;echo $?` -eq '1' ]]; do 
 		echo -n '.'; 
 		sleep 1; 
