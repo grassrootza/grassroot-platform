36ca87a4504b3a9e12e86d994d95cafd2ee5f9ee
Vjeran Marcinko <vmarcinko@gmail.com>
2016-03-04 17:30:15 +0100

    merge USSDGroupUtil changes

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/util/USSDGroupUtil.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/util/USSDGroupUtil.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
-import za.org.grassroot.core.domain.BasePermissions;
 import za.org.grassroot.core.domain.BaseRoles;
 import za.org.grassroot.core.domain.Group;
 import za.org.grassroot.core.domain.Permission;
