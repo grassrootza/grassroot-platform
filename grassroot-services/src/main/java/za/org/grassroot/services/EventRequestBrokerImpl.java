07f08a91c548a37323e47fd338ca9831860d7a2d
Vjeran Marcinko <vmarcinko@gmail.com>
2016-03-21 12:05:00 +0100

    small fix with event description

--- grassroot-services/src/main/java/za/org/grassroot/services/EventRequestBrokerImpl.java
+++ grassroot-services/src/main/java/za/org/grassroot/services/EventRequestBrokerImpl.java
@@ -98,11 +98,11 @@ public class EventRequestBrokerImpl implements EventRequestBroker {
 			MeetingRequest meetingRequest = (MeetingRequest) request;
 			eventBroker.createMeeting(userUid, meetingRequest.getAppliesToGroup().getUid(), meetingRequest.getName(),
 					meetingRequest.getEventStartDateTime(), meetingRequest.getEventLocation(), meetingRequest.isIncludeSubGroups(),
-					meetingRequest.isRsvpRequired(), meetingRequest.isRelayable(), meetingRequest.getReminderType(), meetingRequest.getCustomReminderMinutes(), null);
+					meetingRequest.isRsvpRequired(), meetingRequest.isRelayable(), meetingRequest.getReminderType(), meetingRequest.getCustomReminderMinutes(), meetingRequest.getDescription());
 		} else {
 			VoteRequest voteRequest = (VoteRequest) request;
 			eventBroker.createVote(userUid, voteRequest.getAppliesToGroup().getUid(), voteRequest.getName(),
-					voteRequest.getEventStartDateTime(), voteRequest.isIncludeSubGroups(), voteRequest.isRelayable(), null);
+					voteRequest.getEventStartDateTime(), voteRequest.isIncludeSubGroups(), voteRequest.isRelayable(), voteRequest.getDescription());
 		}
 
 		eventRequestRepository.delete(request);
