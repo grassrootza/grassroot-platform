777306f8689e33fdec6aef6897203543086e3985
Siyanda-Mzam <dot.netntoz@gmail.com>
2016-04-09 19:50:46 +0200

    Introduced a package natty in the project, which has (to our preferences) the customised Lexer and Parser. So I changed the DateGroup and Parser imports references in the DateTimeUtil Class to reference the natty package inside the project. I made the same kind of change to the InputStream Class, Parser, ParserListener, WalkerState Class and the rest in that directory. So that when we make changes to the grammar, lexer or parser, we can easily regenerate the sources, I included natty's pom file. Also changed the headers of the grammar files so that they generate code that has a package referencing natty inside the project. Also changed quite a number of lexing and parsing rules defined in the grammars as well.

--- grassroot-core/src/main/java/za/org/grassroot/core/util/DateTimeUtil.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/util/DateTimeUtil.java
@@ -4,8 +4,8 @@ import com.google.common.base.CharMatcher;
 import com.google.common.base.Joiner;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
-import com.joestelmach.natty.DateGroup;
-import com.joestelmach.natty.Parser;
+import za.org.grassroot.core.util.natty.src.main.java.com.joestelmach.natty.DateGroup;
+import za.org.grassroot.core.util.natty.src.main.java.com.joestelmach.natty.Parser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
