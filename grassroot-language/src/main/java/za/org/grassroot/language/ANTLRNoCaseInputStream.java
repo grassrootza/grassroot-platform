b66e6e111f0ee73687a2cc3be1eb712bd979939c
luke-grassroot <luke.jordan@grassroot.org.za>
2016-04-12 17:30:39 +0200

    Getting language module mostly cleaned up and properly merged, still some way to go

---
+++ grassroot-language/src/main/java/za/org/grassroot/language/ANTLRNoCaseInputStream.java
@@ -0,0 +1,26 @@
+package za.org.grassroot.language;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CharStream;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Extends an ordinary ANTLRInputStream to convert all characters to lower case
+ * @author Joe Stelmach 
+ *
+ */
+public class ANTLRNoCaseInputStream extends ANTLRInputStream {
+  public ANTLRNoCaseInputStream(InputStream inputStream) throws IOException {
+    super(inputStream, null);
+  }
+
+  @Override
+  public int LA(int i) {
+    if (i == 0) return 0;
+    if (i < 0) i++;
+    if ((p + i - 1) >= n) return CharStream.EOF;
+    return Character.toLowerCase(data[p + i - 1]);
+  }
+}
---
+++ grassroot-language/src/test/java/za/org/grassroot/language/grammar/AbstractGrammarTest.java
@@ -0,0 +1,70 @@
+package za.org.grassroot.language.grammar;
+
+import java.io.ByteArrayInputStream;
+import java.lang.reflect.Method;
+
+import junit.framework.Assert;
+
+import org.antlr.runtime.ANTLRInputStream;
+import org.antlr.runtime.CommonTokenStream;
+import org.antlr.runtime.ParserRuleReturnScope;
+import org.antlr.runtime.tree.CommonTree;
+import org.antlr.runtime.tree.CommonTreeNodeStream;
+import org.antlr.runtime.tree.Tree;
+
+import za.org.grassroot.language.ANTLRNoCaseInputStream;
+import za.org.grassroot.language.ParseListener;
+import za.org.grassroot.language.generated.DateLexer;
+import za.org.grassroot.language.generated.DateParser;
+import za.org.grassroot.language.generated.TreeRewrite;
+
+public abstract class AbstractGrammarTest {
+  protected String _ruleName;
+  
+  /**
+   * 
+   * @param input
+   * @param ast
+   */
+  protected void assertAST(String input, String ast) throws Exception {
+    Assert.assertEquals(ast, buildAST(input));
+  }
+  
+  /**
+   * 
+   * @param value
+   * @return
+   * @throws Exception
+   */
+  protected String buildAST(String value) throws Exception {
+    DateParser parser = buildParser(value);
+    Class<?> klass = Class.forName("za.org.grassroot.language.generated.DateParser");
+    Method meth = klass.getMethod(_ruleName, (Class<?>[]) null);
+    ParserRuleReturnScope ret = (ParserRuleReturnScope) meth.invoke(parser, (Object[]) null);
+    
+    Tree tree = (Tree)ret.getTree();
+    // rewrite the tree (temporary fix for http://www.antlr.org/jira/browse/ANTLR-427)
+    CommonTreeNodeStream nodes = new CommonTreeNodeStream(tree);
+    TreeRewrite s = new TreeRewrite(nodes);
+    tree = (CommonTree)s.downup(tree);
+      
+    return tree.toStringTree();
+  }
+  
+  /**
+   * 
+   * @param value
+   * @return
+   */
+  private DateParser buildParser(String value) throws Exception {
+    // lex
+    ANTLRInputStream input = new ANTLRNoCaseInputStream(
+        new ByteArrayInputStream(value.getBytes()));
+    DateLexer lexer = new DateLexer(input);
+    CommonTokenStream tokens = new CommonTokenStream(lexer);
+      
+    // parse 
+    ParseListener listener = new ParseListener();
+    return new DateParser(tokens, listener);
+  }
+}
