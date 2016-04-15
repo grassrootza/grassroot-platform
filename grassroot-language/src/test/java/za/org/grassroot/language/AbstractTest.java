56840e0c9b854e011b830f93910a39e4404a3223
Siyanda-Mzam <dot.netntoz@gmail.com>
2016-04-15 11:51:07 +0200

    Work in progress...

--- grassroot-language/src/test/java/za/org/grassroot/language/AbstractTest.java
+++ grassroot-language/src/test/java/za/org/grassroot/language/AbstractTest.java
@@ -1,19 +1,18 @@
 package za.org.grassroot.language;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 /**
- * 
  * @author Joe Stelmach
  */
 public abstract class AbstractTest {
@@ -22,155 +21,157 @@ public abstract class AbstractTest {
 
     private static final DateTimeFormatter format = DateTimeFormatter.ISO_DATE_TIME;
     private static Calendar _calendar;
-  protected static CalendarSource calendarSource;
-  protected static Parser _parser;
+    protected static CalendarSource calendarSource;
+    protected static Parser _parser;
 
 
-  public static void initCalendarAndParser() {
-    _calendar = Calendar.getInstance();
-    _parser = new Parser();
-  }
+    public static void initCalendarAndParser() {
+        _calendar = Calendar.getInstance();
+        _parser = new Parser();
+    }
 
-  /**
-   * Resets the calendar source time before each test
-   */
-  @Before
-  public void before() {
-    calendarSource = new CalendarSource();
-  }
-  
-  /**
-   * Parses the given value into a collection of dates
-   * 
-   * @param value
-   * @return
-   */
-  protected List<Date> parseCollection(Date referenceDate, String value) {
-    List<DateGroup> dateGroup = _parser.parse(value, referenceDate);
-    return dateGroup.isEmpty() ? new ArrayList<Date>() : dateGroup.get(0).getDates();
-  }
-  
-  /**
-   * Parses the given value, asserting that one and only one date is produced.
-   * 
-   * @param value
-   * @return
-   */
-  protected Date parseSingleDate(String value, Date referenceDate) {
-    List<Date> dates = parseCollection(referenceDate, value);
-    Assert.assertEquals(1, dates.size());
-    return dates.get(0);
-  }
-  
-  /**
-   * Asserts that the given string value parses down to the given 
-   * month, day, and year values.
-   * 
-   * @param value
-   * @param month
-   * @param day
-   * @param year
-   */
-  protected void validateDate(Date referenceDate, String value, int month, int day, int year) {
-    Date date = parseSingleDate(value, referenceDate);
-    validateDate(date, month, day, year);
-  }
+    /**
+     * Resets the calendar source time before each test
+     */
+    @Before
+    public void before() {
+        calendarSource = new CalendarSource();
+    }
 
-  protected void validateDate(String value, int month, int day, int year) {
-    validateDate(new Date(), value, month, day, year);
-  }
-  
-  /**
-   * Asserts that the given date contains the given attributes
-   * 
-   * @param date
-   * @param month
-   * @param day
-   * @param year
-   */
-  protected void validateDate(Date date, int month, int day, int year) {
-    _calendar.setTime(date);
-    Assert.assertEquals(month -1, _calendar.get(Calendar.MONTH));
-    Assert.assertEquals(day, _calendar.get(Calendar.DAY_OF_MONTH));
-    Assert.assertEquals(year, _calendar.get(Calendar.YEAR));
-  }
-  
-  /**
-   * 
-   * Asserts that the given string value parses down to the given 
-   * hours, minutes, and seconds
-   * 
-   * @param value
-   * @param hours
-   * @param minutes
-   * @param seconds
-   */
-  protected void validateTime(Date referenceDate, String value, int hours, int minutes, int seconds) {
-    Date date = parseSingleDate(value, referenceDate);
-    validateTime(date, hours, minutes, seconds);
-  }
-  
-  /**
-   * 
-   * Asserts that the given date contains the given time attributes
-   * 
-   * @param date 
-   * @param hours
-   * @param minutes
-   * @param seconds
-   */
-  protected void validateTime(Date date, int hours, int minutes, int seconds) {
-    _calendar.setTime(date);
-    Assert.assertEquals(hours, _calendar.get(Calendar.HOUR_OF_DAY));
-    Assert.assertEquals(minutes, _calendar.get(Calendar.MINUTE));
-    Assert.assertEquals(seconds, _calendar.get(Calendar.SECOND));
-  }
-  
-  /**
-   * 
-   * Asserts that the given string value parses down to the given 
-   * month, day, year, hours, minutes, and seconds
-   * 
-   * @param value
-   * @param month
-   * @param day
-   * @param year
-   * @param hours
-   * @param minutes
-   * @param seconds
-   */
-  protected void validateDateTime(Date referenceDate, String value, int month, int day, int year,
-      int hours, int minutes, int seconds) {
+    /**
+     * Parses the given value into a collection of dates
+     *
+     * @param value
+     * @return
+     */
+    protected List<Date> parseCollection(Date referenceDate, String value) {
+        log.debug("Inside the AbstractTest Class...about to parse this -> " + value);
+        List<DateGroup> dateGroup = _parser.parse(value, referenceDate);
+        log.debug("Just parsed it... Got a list of date groups with size " + dateGroup.size());
+        return dateGroup.isEmpty() ? new ArrayList<Date>() : dateGroup.get(0).getDates();
+    }
 
-      System.out.println("Trying to parse string: " + value);
-    
-    Date date = parseSingleDate(value, referenceDate);
-    validateDateTime(date, month, day, year, hours, minutes, seconds);
-  }
-  
-  /**
-   * Asserts that the given date contains the given attributes
-   * 
-   * @param date
-   * @param month
-   * @param day
-   * @param year
-   * @param hours
-   * @param minutes
-   * @param seconds
-   */
-  protected void validateDateTime(Date date, int month, int day, int year, 
-      int hours, int minutes, int seconds) {
+    /**
+     * Parses the given value, asserting that one and only one date is produced.
+     *
+     * @param value
+     * @return
+     */
+    protected Date parseSingleDate(String value, Date referenceDate) {
+        log.debug("Inside parsingSingleDate...about to thoroughly examine {} for \"dates\"...", value);
+        List<Date> dates =  parseCollection(referenceDate, value);
+        log.debug("Done!!! Works of a beautiful parser look like this -> " + dates);
+        assertEquals(1, dates.size());
+        return dates.get(0);
+    }
 
-      System.out.println(String.format("Date passed, back as: %s, expecting month %d and day %d",
-                                       date.toInstant().toString(), month, day));
+    /**
+     * Asserts that the given string value parses down to the given
+     * month, day, and year values.
+     *
+     * @param value
+     * @param month
+     * @param day
+     * @param year
+     */
+    protected void validateDate(Date referenceDate, String value, int day, int month, int year) {
+        Date date = parseSingleDate(value, referenceDate);
+        validateDate(date, month, day, year);
+    }
 
-      _calendar.setTime(date);
-    Assert.assertEquals(month -1, _calendar.get(Calendar.MONTH));
-    Assert.assertEquals(day, _calendar.get(Calendar.DAY_OF_MONTH));
-    Assert.assertEquals(year, _calendar.get(Calendar.YEAR));
-    Assert.assertEquals(hours, _calendar.get(Calendar.HOUR_OF_DAY));
-    Assert.assertEquals(minutes, _calendar.get(Calendar.MINUTE));
-    Assert.assertEquals(seconds, _calendar.get(Calendar.SECOND));
-  }
+    protected void validateDate(String value, int day, int month, int year) {
+        validateDate(new Date(), value, day, month, year);
+    }
+
+    /**
+     * Asserts that the given date contains the given attributes
+     *
+     * @param date
+     * @param month
+     * @param day
+     * @param year
+     */
+    protected void validateDate(Date date, int day, int month, int year) {
+        _calendar.setTime(date);
+        assertEquals(month - 1, _calendar.get(Calendar.MONTH));
+        assertEquals(day, _calendar.get(Calendar.DAY_OF_MONTH));
+        assertEquals(year, _calendar.get(Calendar.YEAR));
+    }
+
+    /**
+     * Asserts that the given string value parses down to the given
+     * hours, minutes, and seconds
+     *
+     * @param value
+     * @param hours
+     * @param minutes
+     * @param seconds
+     */
+    protected void validateTime(Date referenceDate, String value, int hours, int minutes, int seconds) {
+        Date date = parseSingleDate(value, referenceDate);
+        validateTime(date, hours, minutes, seconds);
+    }
+
+    /**
+     * Asserts that the given date contains the given time attributes
+     *
+     * @param date
+     * @param hours
+     * @param minutes
+     * @param seconds
+     */
+    protected void validateTime(Date date, int hours, int minutes, int seconds) {
+        _calendar.setTime(date);
+        assertEquals(hours, _calendar.get(Calendar.HOUR_OF_DAY));
+        assertEquals(minutes, _calendar.get(Calendar.MINUTE));
+        assertEquals(seconds, _calendar.get(Calendar.SECOND));
+    }
+
+    /**
+     * Asserts that the given string value parses down to the given
+     * month, day, year, hours, minutes, and seconds
+     *
+     * @param value
+     * @param month
+     * @param day
+     * @param year
+     * @param hours
+     * @param minutes
+     * @param seconds
+     */
+    protected void validateDateTime(Date referenceDate, String value, int day, int month, int year,
+                                    int hours, int minutes, int seconds) {
+
+        log.debug("Trying to parse string: " + value);
+
+        Date date = parseSingleDate(value, referenceDate);
+        log.debug("Tried to parse and now I got back this: " + date.toString());
+        validateDateTime(date, day, month, year, hours, minutes, seconds);
+    }
+
+    /**
+     * Asserts that the given date contains the given attributes
+     *
+     * @param date
+     * @param month
+     * @param day
+     * @param year
+     * @param hours
+     * @param minutes
+     * @param seconds
+     */
+    protected void validateDateTime(Date date, int day, int month, int year,
+                                    int hours, int minutes, int seconds) {
+
+        log.debug(String.format("Date passed, back as: %s, expecting month number %d and day number %d",
+                date.toString(), month, day));
+
+        _calendar.setTime(date);
+        assertEquals(month - 1, _calendar.get(Calendar.MONTH));
+        assertEquals(day, _calendar.get(Calendar.DAY_OF_MONTH));
+        assertEquals(year, _calendar.get(Calendar.YEAR));
+        assertEquals(hours, _calendar.get(Calendar.HOUR_OF_DAY));
+        assertEquals(minutes, _calendar.get(Calendar.MINUTE));
+        assertEquals(seconds, _calendar.get(Calendar.SECOND));
+    }
 }
