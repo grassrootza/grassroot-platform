1679bab0ca9d81fb7b2e79414c488feaa18e898b
luke-grassroot <luke.jordan@grassroot.org.za>
2016-04-29 12:09:21 +0200

    Antlr AWS heap size hopefully now fixed

--- scripts/compilegrassroot.sh
+++ scripts/compilegrassroot.sh
@@ -3,8 +3,5 @@
 . /etc/environment
 #cd /var/grassroot
 cd /opt/codedeploy-agent/deployment-root/${DEPLOYMENT_GROUP_ID}/${DEPLOYMENT_ID}/deployment-archive
-mvn -e clean
-cd ./grassroot-language/
-mvn -Dmaven.test.skip=true -e generate-sources
-cd ../
+mvn -e clean && mvn -Dmaven.test.skip=true -e generate-sources
 mvn -Dmaven.test.skip=true -e compile && mvn -Dmaven.test.skip=true install
