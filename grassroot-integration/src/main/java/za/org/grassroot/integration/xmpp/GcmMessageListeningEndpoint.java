68bdbcd50c0af95388ea2e6d3ca60eea4291b44a
Paballo Ditshego <pdditshego@gmail.com>
2016-05-10 16:02:38 +0200

    Created new class for to be used to marshall upstream messages; Added script to alter registration_id column length in gcm_registration_table; Made changes in the authentication method of the check if user has android profile.

--- grassroot-integration/src/main/java/za/org/grassroot/integration/xmpp/GcmMessageListeningEndpoint.java
+++ grassroot-integration/src/main/java/za/org/grassroot/integration/xmpp/GcmMessageListeningEndpoint.java
@@ -1,15 +1,24 @@
 package za.org.grassroot.integration.xmpp;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.log4j.Logger;
 import org.jivesoftware.smack.PacketListener;
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.packet.Packet;
+import org.jivesoftware.smack.packet.PacketExtension;
+import org.jivesoftware.smack.provider.PacketExtensionProvider;
+import org.jivesoftware.smack.provider.ProviderManager;
 import org.springframework.integration.support.MessageBuilder;
 import org.springframework.integration.xmpp.inbound.ChatMessageListeningEndpoint;
 import org.springframework.integration.xmpp.support.DefaultXmppHeaderMapper;
 import org.springframework.integration.xmpp.support.XmppHeaderMapper;
+import org.xmlpull.v1.XmlPullParser;
+import za.org.grassroot.integration.domain.GcmEntity;
+import za.org.grassroot.integration.domain.GcmUpstreamMessage;
 
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -21,9 +30,25 @@ public class GcmMessageListeningEndpoint extends ChatMessageListeningEndpoint {
     private Logger log = Logger.getLogger(GcmMessageListeningEndpoint.class);
     protected PacketListener packetListener = new GcmPacketListener();
     protected XmppHeaderMapper headerMapper = new DefaultXmppHeaderMapper();
+    private ObjectMapper mapper = new ObjectMapper();
 
     public GcmMessageListeningEndpoint(XMPPConnection connection) {
         super(connection);
+        ProviderManager.addExtensionProvider(GcmPacketExtension.GCM_ELEMENT_NAME, GcmPacketExtension.GCM_NAMESPACE,
+
+                new PacketExtensionProvider() {
+
+                    @Override
+
+                    public PacketExtension parseExtension(XmlPullParser parser) throws Exception {
+
+                        String json = parser.nextText();
+
+                        return new GcmPacketExtension(json);
+
+                    }
+
+                });
 
     }
 
@@ -31,7 +56,7 @@ public class GcmMessageListeningEndpoint extends ChatMessageListeningEndpoint {
         return "xmpp:inbound-channel-adapter-gcm";
     }
 
-    @Override
+   @Override
     public void setHeaderMapper(XmppHeaderMapper headerMapper) {
         super.setHeaderMapper(headerMapper);
         this.headerMapper = headerMapper;
@@ -56,9 +81,17 @@ public class GcmMessageListeningEndpoint extends ChatMessageListeningEndpoint {
             log.info("Packet received from gcm " + packet.toString());
             if (packet instanceof org.jivesoftware.smack.packet.Message) {
                 org.jivesoftware.smack.packet.Message xmppMessage = (org.jivesoftware.smack.packet.Message) packet;
+                final GcmPacketExtension gcmExtension = (GcmPacketExtension)xmppMessage.getExtension(GcmPacketExtension.GCM_NAMESPACE);
                 log.info("Message received: " + xmppMessage.toXML().toString());
-                Map<String, Object> mappedHeaders = headerMapper.toHeadersFromRequest(xmppMessage);
-                sendMessage(MessageBuilder.withPayload(xmppMessage).copyHeaders(mappedHeaders).build());
+                try {
+                    mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+                    final GcmUpstreamMessage entity = mapper.readValue(gcmExtension.getJson(), GcmUpstreamMessage.class);
+                    Map<String, Object> mappedHeaders = headerMapper.toHeadersFromRequest(xmppMessage);
+                    sendMessage(MessageBuilder.withPayload(entity).copyHeaders(mappedHeaders).build());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             } else {
 
                 log.warn("Unsuported Packet " + packet.toString());
--- grassroot-webapp/src/main/resources/db/scripts/script_86.sql
+++ grassroot-webapp/src/main/resources/db/scripts/script_86.sql
@@ -1,1 +1,1 @@
-alter table gcm_registration alter column registration_id character VARYING (255);
+alter table gcm_registration alter column registration_id TYPE character VARYING(255);
