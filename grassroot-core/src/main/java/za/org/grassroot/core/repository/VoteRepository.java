397f0703b296bf368181da6661c14cd52c13f248
luke-grassroot <luke.jordan@grassroot.org.za>
2016-06-16 21:01:35 +0200

    Transactional annotation on one repo

--- grassroot-core/src/main/java/za/org/grassroot/core/repository/VoteRepository.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/repository/VoteRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 import za.org.grassroot.core.domain.Event;
 import za.org.grassroot.core.domain.Group;
 import za.org.grassroot.core.domain.User;
@@ -18,6 +19,7 @@ public interface VoteRepository extends JpaRepository<Vote, Long> {
 
 	int countByCreatedDateTimeBetween(Instant start, Instant end);
 
+	@Transactional
 	@Query(value = "SELECT * FROM event e WHERE start_date_time  between  (current_timestamp - INTERVAL '1 hour') and current_timestamp AND e.type = 'VOTE' AND e.canceled = FALSE AND (SELECT count(*) FROM event_log el WHERE el.event_log_type = 7 AND e.id = el.event_id) = 0", nativeQuery = true)
 	List<Vote> findUnsentVoteResults();
 
