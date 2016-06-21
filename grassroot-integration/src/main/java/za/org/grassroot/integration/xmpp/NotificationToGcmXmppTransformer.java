4a59de903e781d97ce29a49c826b829f7a95bd90
Paballo Ditshego <pdditshego@gmail.com>
2016-06-21 17:22:49 +0200

    Minor fix to the NotificationGcmXmppTransformer, must retrieve the logbook's uid instead.

--- grassroot-integration/src/main/java/za/org/grassroot/integration/xmpp/NotificationToGcmXmppTransformer.java
+++ grassroot-integration/src/main/java/za/org/grassroot/integration/xmpp/NotificationToGcmXmppTransformer.java
@@ -128,7 +128,7 @@ public class NotificationToGcmXmppTransformer {
                         logBook.getAncestorGroup().getGroupName(),
                         null,
                         notification.getMessage(),
-                        notification.getLogBookLog().getLogBook().getParent().getUid(),
+                        logBook.getUid(),
                         notification.getCreatedDateTime(),
                         notification.getNotificationType(), TaskType.TODO.name()
                 );
