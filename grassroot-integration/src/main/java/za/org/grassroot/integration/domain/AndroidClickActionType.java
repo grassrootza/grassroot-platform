bcecab75b3eb647dc04cd5e5200bf08cb3f86aa0
Luke Jordan <luke.jordan@grassroot.org.za>
2016-07-27 13:29:44 +0200

    Added action type for Android notifications

---
+++ grassroot-integration/src/main/java/za/org/grassroot/integration/domain/AndroidClickActionType.java
@@ -0,0 +1,14 @@
+package za.org.grassroot.integration.domain;
+
+/**
+ * Created by luke on 2016/07/27.
+ */
+public enum AndroidClickActionType {
+
+	VIEW_TASK,
+	TASK_CREATED,
+	TASK_REMINDER,
+	TASK_CHANGED,
+	TASK_CANCELLED
+
+}
