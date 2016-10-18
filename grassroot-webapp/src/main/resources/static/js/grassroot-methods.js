e8075817e0b8ac3e2b2d92ca5e94a77b4b7e5577
Luke Jordan <luke.jordan@grassroot.org.za>
2016-10-18 11:32:29 +0200

    Pushing forward with webapp UI fixes / CSS cleaning

--- grassroot-webapp/src/main/resources/static/js/grassroot-methods.js
+++ grassroot-webapp/src/main/resources/static/js/grassroot-methods.js
@@ -8,22 +8,22 @@ var grassrootJS = {
         var strVar="";
         strVar += "<tr class=\"member_input_row\">";
         strVar += "            <td>";
-        strVar += "              <input type=\"text\" class=\"form-control\" value=\"\" id=\"" + listName + newMemberIndex + ".displayName\" " +
+        strVar += "              <input type=\"text\" class=\"form-control input-lg\" value=\"\" id=\"" + listName + newMemberIndex + ".displayName\" " +
             "name=\"" + listName + "[" + newMemberIndex + "].displayName\" maxlength='25' \/>";
         strVar += "            <\/td>";
         strVar += "            <td>";
-        strVar += "              <input type=\"text\" class=\"form-control\" value=\"\" id=\"" + listName + newMemberIndex + ".phoneNumber\" name=\"" + listName + "[" + newMemberIndex + "].phoneNumber\" \/>";
+        strVar += "              <input type=\"text\" class=\"form-control input-lg\" value=\"\" id=\"" + listName + newMemberIndex + ".phoneNumber\" name=\"" + listName + "[" + newMemberIndex + "].phoneNumber\" \/>";
         strVar += "              ";
         strVar += "            <\/td>";
         strVar += "            <td>";
-        strVar += "              <select class=\"form-control\" id=\"" + listName + newMemberIndex + ".roleName\" name=\"" + listName + "[" + newMemberIndex + "].roleName\">";
+        strVar += "              <select class=\"form-control input-lg\" id=\"" + listName + newMemberIndex + ".roleName\" name=\"" + listName + "[" + newMemberIndex + "].roleName\">";
         strVar += "                <option selected=\"selected\" value=\"ROLE_ORDINARY_MEMBER\">Ordinary member<\/option>";
         strVar += "                <option value=\"ROLE_COMMITTEE_MEMBER\">Committee member<\/option>";
         strVar += "                <option value=\"ROLE_GROUP_ORGANIZER\">Group organizer<\/option>";
         strVar += "              <\/select>";
         strVar += "            <\/td>";
         strVar += "            <td>";
-        strVar += "              <button type=\"button\" id=\"removeMember\" name=\"removeMember\" class=\"btn btn-default\" aria-label=\"Remove\" value=\"" + newMemberIndex + "\">";
+        strVar += "              <button type=\"button\" id=\"removeMember\" name=\"removeMember\" class=\"btn btn-default btn-lg\" aria-label=\"Remove\" value=\"" + newMemberIndex + "\">";
         strVar += "                <span class=\"glyphicon glyphicon-remove\" aria-hidden=\"true\"><\/span>";
         strVar += "              <\/button>";
         strVar += "            <\/td>";
