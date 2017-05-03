b0b4918ebd851efcf706868ceaaf0f5ffc61c8f7
Mauricio Ferreira <malf@visiansystems.com>
2017-05-03 09:46:33 +0100

    Added InvalidGeoLocationException.

---
+++ grassroot-services/src/main/java/za/org/grassroot/services/exception/InvalidGeoLocationException.java
@@ -0,0 +1,7 @@
+package za.org.grassroot.services.exception;
+
+public class InvalidGeoLocationException extends Exception {
+    public InvalidGeoLocationException (String message) {
+        super(message);
+    }
+}
