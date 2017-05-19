5ca6e10a6edaefdc9c0523050c7971f110b99865
Mauricio Ferreira <malf@visiansystems.com>
2017-05-19 10:27:21 +0100

    Added missing RestMessage entry.

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/webapp/LocationController.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/webapp/LocationController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
