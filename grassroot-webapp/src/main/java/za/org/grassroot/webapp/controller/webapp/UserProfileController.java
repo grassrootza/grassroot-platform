4d1e780551291e8023bde08ffd101a088a149158
Luke Jordan <luke-grassroot@users.noreply.github.com>
2017-09-28 08:21:09 +0200

    Group export, per-message account features (#198)
    * Group members export to excel (GCC-16) implemented. Not done yet, some UI tweaks needed.
    
    * [WIP] per message account started
    
    * Finished off adding per-message billing modifications and bill generation
    
    * Group members export finished
    
    * Group members export: displayName used for user name column
    
    * Group members export: bug fixes
    
    * Group members export: more bug fixes and tweaks

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/webapp/UserProfileController.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/webapp/UserProfileController.java
@@ -1,5 +1,6 @@
 package za.org.grassroot.webapp.controller.webapp;
 
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,18 +8,27 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import za.org.grassroot.core.domain.Group;
 import za.org.grassroot.core.domain.User;
 import za.org.grassroot.services.exception.InvalidTokenException;
+import za.org.grassroot.services.group.GroupExportBroker;
 import za.org.grassroot.services.user.UserManagementService;
 import za.org.grassroot.webapp.controller.BaseController;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 /**
  * @author Lesetse Kimwaga
@@ -30,10 +40,13 @@ public class UserProfileController extends BaseController {
     private static final Logger log = LoggerFactory.getLogger(UserProfileController.class);
 
     private final UserManagementService userManagementService;
+    private GroupExportBroker groupExportBroker;
+
 
     @Autowired
-    public UserProfileController(UserManagementService userManagementService) {
+    public UserProfileController(UserManagementService userManagementService, GroupExportBroker groupExportBroker) {
         this.userManagementService = userManagementService;
+        this.groupExportBroker = groupExportBroker;
     }
 
     @ModelAttribute("sessionUser")
@@ -81,5 +94,39 @@ public class UserProfileController extends BaseController {
             return "user/password";
         }
     }
+
+
+    @RequestMapping(value = "export-groups", method = RequestMethod.GET)
+    public String exportGroupsPrompt(Model model) {
+
+        User user = getUserProfile();
+        user = userManagementService.load(user.getUid()); // take fresh copy in order to have groups added since login
+        List<Group> activeGroups = user.getGroups().stream().filter(Group::isActive).sorted(Comparator.comparing(Group::getGroupName)).collect(Collectors.toList());
+        model.addAttribute("groups", activeGroups);
+        return "user/export-groups";
+    }
+
+    @RequestMapping(value = "export-groups", method = RequestMethod.POST)
+    public void exportGroupsDo(@RequestParam String[] selectedGroupUids, HttpServletResponse response) throws IOException {
+
+        User user = getUserProfile();
+        user = userManagementService.load(user.getUid()); // take fresh copy in order to have groups added since login
+        List<Group> userGroups = user.getGroups().stream().filter(Group::isActive).sorted(Comparator.comparing(Group::getGroupName)).collect(Collectors.toList());
+        List<String> userGroupUids = userGroups.stream().map(Group::getUid).collect(Collectors.toList());
+
+        XSSFWorkbook xls = groupExportBroker.exportMultipleGroupMembers(userGroupUids, Arrays.asList(selectedGroupUids));
+
+        String fileName = "multiple_group_members.xlsx";
+        response.setContentType(MimeTypeUtils.APPLICATION_OCTET_STREAM_VALUE);
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+
+        xls.write(response.getOutputStream());
+        response.flushBuffer();
+    }
+
+
 
 }
