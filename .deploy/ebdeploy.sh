247571e599c0821b57f2474851a977e025941de7
digitalassembly <carlos@digitalassemb.ly>
2017-11-01 20:41:03 +1000

    fix deploy looping old version

--- .deploy/ebdeploy.sh
+++ .deploy/ebdeploy.sh
@@ -27,14 +27,14 @@ echo $COMMIT_MESSAGE > deploy_status.txt
 
 # COMMIT
 # thats just a "virtual" commit since we do not send anything back to the repository, but this is requred for beasntalke to get the latest staged version to be deployed
-git config --global user.email "grassroot@digitalassemb.ly"
+git config --global user.email "grassroot@grassroot.com"
 git config --global user.name "GRASSROOT"
 git add .
-git commit -m "$ENVIRONMENT-$SHA1"
+git commit -m "$ENVIRONMENT-$COMMIT_MESSAGE"
 
 # DEPLOY APP
 mv .deploy/.elasticbeanstalk .elasticbeanstalk
 mv .deploy/.ebignore .ebignore
 mv .deploy/Dockerrun.aws.json Dockerrun.aws.json
 eb use $EBENVIRONMENT
-eb deploy $EBENVIRONMENT --label "$ENVIRONMENT-$SHA1"
+eb deploy $EBENVIRONMENT --label "$ENVIRONMENT-$COMMIT_MESSAGE"
