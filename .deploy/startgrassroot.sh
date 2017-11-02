d92148dc2e1aec2167805972f161deaf6382f2b9
mugich <anikievev@gmail.com>
2017-11-02 15:43:09 +0300

    change hostname

--- .deploy/startgrassroot.sh
+++ .deploy/startgrassroot.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+# SET ENVIRONMENT VARIABLES BASED ON THE ENVIRONMENT PREVIOUSLY SETUP VIA
+. /usr/src/grassroot/environment/environment-variables
+
+wget http://s3.amazonaws.com/ec2metadata/ec2-metadata
+chmod 777 ec2-metadata
+INSTANCE_ID=$(./ec2-metadata -i | cut -b 14-)
+HOSTNAME=$PROFILE$INSTANCE_ID
+echo $HOSTNAME > /etc/hostname
+
 # SETUP PAPERTRAIL
 wget -qO - --header="X-Papertrail-Token: zZJTlxJZTvV5r18UunSF" https://papertrailapp.com/destinations/6213432/setup.sh | bash
 wget https://github.com/papertrail/remote_syslog2/releases/download/v0.19/remote-syslog2_0.19_amd64.deb
 dpkg -i remote-syslog2_0.19_amd64.deb
 remote_syslog -p 48972 -d logs6.papertrailapp.com --pid-file=/var/run/remote_syslog.pid  /usr/src/grassroot/log/grassroot-app.log
-
-# SET ENVIRONMENT VARIABLES BASED ON THE ENVIRONMENT PREVIOUSLY SETUP VIA
-. /usr/src/grassroot/environment/environment-variables
 
 # START GRASSROOT
 cd /usr/src/grassroot/
