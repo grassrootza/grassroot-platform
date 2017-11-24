00ae9b24e0f6d56d4eb0a139cfc99fbdab6170d4
Luke Jordan <luke-grassroot@users.noreply.github.com>
2017-11-24 09:40:47 +0200

    Added create vote REST methods, emoji removal on names (for SMS handling)
    * Task create
    
    * Create vote
    
    * Task creation and its test
    
    * Task create
    
    * Emoji removal
    
    * Emoji and unwanted character removal

--- grassroot-core/src/main/java/za/org/grassroot/core/domain/Group.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/domain/Group.java
@@ -8,10 +8,12 @@ import za.org.grassroot.core.domain.geo.GroupLocation;
 import za.org.grassroot.core.domain.task.*;
 import za.org.grassroot.core.enums.GroupDefaultImage;
 import za.org.grassroot.core.util.DateTimeUtil;
+import za.org.grassroot.core.util.FormatUtil;
 import za.org.grassroot.core.util.UIDGenerator;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 import java.util.Collection;
@@ -19,7 +21,11 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+
+import static za.org.grassroot.core.util.FormatUtil.removeUnwantedCharacters;
 
 @Entity
 @Table(name = "group_profile") // quoting table name in case "group" is a reserved keyword
@@ -174,8 +180,9 @@ public class Group implements TodoContainer, VoteContainer, MeetingContainer, Se
     }
 
     public Group(String groupName, User createdByUser, Group parent) {
+        Objects.requireNonNull(groupName);
         this.uid = UIDGenerator.generateId();
-        this.groupName = Objects.requireNonNull(groupName);
+        this.groupName = removeUnwantedCharacters(groupName);
         this.createdByUser = Objects.requireNonNull(createdByUser);
         this.createdDateTime = Instant.now();
         this.lastGroupChangeTime = this.createdDateTime;
@@ -229,7 +236,7 @@ public class Group implements TodoContainer, VoteContainer, MeetingContainer, Se
     }
 
     public void setGroupName(String groupName) {
-        this.groupName = groupName;
+        this.groupName = removeUnwantedCharacters(groupName);
     }
 
     public Long getId() {
@@ -700,7 +707,4 @@ public class Group implements TodoContainer, VoteContainer, MeetingContainer, Se
             return createdDateTime.compareTo(otherCreatedDateTime);
         }
     }
-
-
-
 }
