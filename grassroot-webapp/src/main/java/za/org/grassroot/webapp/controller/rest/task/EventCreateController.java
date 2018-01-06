ef6f52571de1d64e74e695f3e5d5a9bfa21efb8a
Mato <mato.stanic@inovatrend.com>
2018-01-06 22:22:54 +0100

    Changed EventCreateController. Changes for creating meeting and vote for groups.

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/rest/task/EventCreateController.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/rest/task/EventCreateController.java
@@ -16,17 +16,21 @@ import za.org.grassroot.core.domain.geo.GeoLocation;
 import za.org.grassroot.core.domain.task.Meeting;
 import za.org.grassroot.core.domain.task.Vote;
 import za.org.grassroot.core.dto.task.TaskFullDTO;
+import za.org.grassroot.integration.messaging.JwtService;
 import za.org.grassroot.services.exception.MemberLacksPermissionException;
 import za.org.grassroot.services.task.EventBroker;
 import za.org.grassroot.services.task.MeetingBuilderHelper;
 import za.org.grassroot.services.task.TaskImageBroker;
 import za.org.grassroot.services.user.UserManagementService;
+import za.org.grassroot.webapp.controller.rest.BaseRestController;
 import za.org.grassroot.webapp.controller.rest.Grassroot2RestController;
 import za.org.grassroot.webapp.enums.VoteType;
 import za.org.grassroot.webapp.model.web.VoteWrapper;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
@@ -37,21 +41,23 @@ import static za.org.grassroot.core.util.DateTimeUtil.getPreferredRestFormat;
 @RestController @Grassroot2RestController
 @Api("/api/task/create/meeting")
 @RequestMapping(value = "/api/task/create/")
-public class EventCreateController {
+public class EventCreateController extends BaseRestController{
 
     private final EventBroker eventBroker;
     private final UserManagementService userService;
 
     @Autowired
-    public EventCreateController(UserManagementService userManagementService, EventBroker eventBroker, TaskImageBroker taskImageBroker) {
+    public EventCreateController(JwtService jwtService, UserManagementService userManagementService,
+                                 EventBroker eventBroker, TaskImageBroker taskImageBroker) {
+        super(jwtService, userManagementService);
         this.userService = userManagementService;
         this.eventBroker = eventBroker;
     }
 
-    @RequestMapping(value = "/meeting/{userUid}/{parentType}/{parentUid}", method = RequestMethod.POST)
+    @RequestMapping(value = "/meeting/{parentType}/{parentUid}", method = RequestMethod.POST)
     @ApiOperation(value = "Call a meeting", notes = "Creates a meeting, that starts at the specified date and time, passed in " +
-            "as epoch millis. The first six params are necessary, the rest are optional")
-    public ResponseEntity<TaskFullDTO> createMeeting(@PathVariable String userUid,
+            "as epoch millis. The first five params are necessary, the rest are optional")
+    public ResponseEntity<TaskFullDTO> createMeeting(HttpServletRequest request,
                                                      @PathVariable String parentUid,
                                                      @PathVariable JpaEntityType parentType,
                                                      @RequestParam String subject,
@@ -66,6 +72,7 @@ public class EventCreateController {
                                                      @RequestParam(required = false)
                                                      @ApiParam(value = "Server UID of an optional image to include " +
                                                              "in the meeting call") String mediaFileUid) {
+        String userUid = getUserIdFromRequest(request);
         log.info("creating a meeting, subject ={}, location = {}, parentUid = {}", subject, location,parentUid);
 
         MeetingBuilderHelper helper = new MeetingBuilderHelper()
@@ -102,35 +109,34 @@ public class EventCreateController {
         }
     }
 
-    @RequestMapping(value = "/vote/{userUid}/{parentType}/{parentUid}", method = RequestMethod.POST)
+    @RequestMapping(value = "/vote/{parentType}/{parentUid}", method = RequestMethod.POST)
     @ApiOperation(value = "Call a vote", notes = "Creates a vote, that starts at the specified date and time, passed in " +
-            "as epoch millis. The first six params are necessary, the rest are optional")
-    public ResponseEntity<TaskFullDTO> createVote(@PathVariable String userUid,
+            "as epoch millis. The first five params are necessary, the rest are optional")
+    public ResponseEntity<TaskFullDTO> createVote(HttpServletRequest request,
                                                   @PathVariable String parentUid,
                                                   @PathVariable JpaEntityType parentType,
                                                   @RequestParam String title,
                                                   @RequestParam(required = false) List<String> voteOptions,
                                                   @RequestParam(required = false) String description,
-                                                  @RequestParam String time,
+                                                  @RequestParam long time,
                                                   @RequestParam(required = false)
                                                       @ApiParam(value = "UIDs of assigned members, if left blank all " +
                                                               "members of the parent are assigned") Set<String> assignedMemberUids){
 
-        LocalDateTime eventStartDateTime = LocalDateTime.parse(time.trim(), getPreferredRestFormat());
+        String userUid = getUserIdFromRequest(request);
+        LocalDateTime eventStartDateTime = Instant.ofEpochMilli(time).atZone(ZoneId.systemDefault()).toLocalDateTime();
         VoteWrapper voteWrapper = VoteWrapper.makeEmpty();
 
         try{
             User user = userService.load(userUid);
             voteWrapper.setParentUid(parentUid);
 
-            List<String> options = VoteType.YES_NO.equals(voteWrapper.getType()) ? null : voteOptions;
-
             assignedMemberUids = assignedMemberUids == null ? Collections.emptySet() : assignedMemberUids;
 
             log.info("title={}, description={}, time={}, members={}, options={}",title,description,time,assignedMemberUids, voteOptions);
 
             Vote vote = eventBroker.createVote(user.getUid(), parentUid, parentType, title, eventStartDateTime,
-                    false, description, assignedMemberUids, options);
+                    false, description, assignedMemberUids, voteOptions);
 
             log.debug("Vote={},User={}",vote,user);
             return ResponseEntity.ok(new TaskFullDTO(vote, user,vote.getCreatedDateTime(), null));
