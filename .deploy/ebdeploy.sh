b91d1ab7303b48a0079e09d3c34389c4f08d2b28
Carlos Santini <carlos@digitalassemb.ly>
2018-02-08 22:44:43 +1000

    nginx logs redirection 2

--- .deploy/ebdeploy.sh
+++ .deploy/ebdeploy.sh
@@ -30,6 +30,12 @@ aws s3 cp s3://$S3BUCKET/pdf_templates/ templates/pdf/ --region $S3REGION --recu
 echo $COMMIT_MESSAGE > deploy_status.txt
 
 # COMMIT
+mv .deploy/.elasticbeanstalk .elasticbeanstalk
+mv .deploy/.ebextensions .ebextensions
+mv .deploy/.ebignore .ebignore
+mv .deploy/Dockerrun.aws.json Dockerrun.aws.json
+mv .deploy/log_files.yml log_files.yml
+
 # thats just a "virtual" commit since we do not send anything back to the repository, but this is requred for beasntalke to get the latest staged version to be deployed
 git config --global user.email "grassroot@grassroot.com"
 git config --global user.name "GRASSROOT"
@@ -37,10 +43,5 @@ git add .
 git commit -m "$ENVIRONMENT-$COMMIT_MESSAGE"
 
 # DEPLOY APP
-mv .deploy/.elasticbeanstalk .elasticbeanstalk
-mv .deploy/.ebextensions .ebextensions
-mv .deploy/.ebignore .ebignore
-mv .deploy/Dockerrun.aws.json Dockerrun.aws.json
-mv .deploy/log_files.yml log_files.yml
 eb use $EBENVIRONMENT
 eb deploy $EBENVIRONMENT --label "$ENVIRONMENT-$COMMIT_MESSAGE" --timeout 20
