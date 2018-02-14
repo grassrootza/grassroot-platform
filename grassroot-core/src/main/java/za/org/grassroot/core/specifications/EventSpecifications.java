9b686793ee2b957b63e4808d0a40b5ac0bffbf76
Luke Jordan <luke-grassroot@users.noreply.github.com>
2018-02-14 22:35:16 +0200

    Adjusted new event searching, also some updates to language file
    * Attempting solution to CORS issue
    
    * Fixed a bug on email only member importing, and made range of tweaks to broadcasts
    
    * Range of fixes on broadcasts, added cache to social media responses
    
    * Fixes on member adding, filtering
    
    * changes made to port live wire (#266)
    
    * Range of changes for task teams, topic setting
    
    * Overhaul and improvement of isiZulu translations, step 1
    
    * Bug fix for sending some messages, version bump to align core
    
    * Updating various language files
    
    * Switched upcoming events to specs, filtered for non-assignment, also upped core dep versions

--- grassroot-core/src/main/java/za/org/grassroot/core/specifications/EventSpecifications.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/specifications/EventSpecifications.java
@@ -1,10 +1,12 @@
 package za.org.grassroot.core.specifications;
 
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.data.jpa.domain.Specifications;
 import za.org.grassroot.core.domain.*;
 import za.org.grassroot.core.domain.task.AbstractEventEntity_;
 import za.org.grassroot.core.domain.task.Event;
 import za.org.grassroot.core.domain.task.Event_;
+import za.org.grassroot.core.enums.EventType;
 
 import javax.persistence.criteria.Join;
 import java.time.Instant;
@@ -15,6 +17,33 @@ import java.util.Collection;
  * Unfortunately it looks non-trivial to create a spec for the discriminator type ...
  */
 public final class EventSpecifications {
+
+    public static Specifications<Event> upcomingEventsForUser(User user) {
+        Specification<Event> basicProperties = (root, query, cb) -> cb.and(cb.isFalse(root.get("canceled")), cb.isTrue(root.get("rsvpRequired")),
+                cb.greaterThan(root.get("eventStartDateTime"), Instant.now()));
+        Specification<Event> userInAncestorGroup = (root, query, cb) -> {
+            Join<Event, User> userGroupJoin = root.join("parentGroup").join("memberships");
+            return cb.equal(userGroupJoin.get("user"), user);
+        };
+
+        //N.B. remove this if statement if you want to allow votes for people that joined the group late
+        Specification<Event> userJoinedAfterVote = (root, query, cb) -> {
+            Join<Event, Membership> parentGroupMembership = root.join("ancestorGroup").join("memberships");
+            parentGroupMembership.on(cb.equal(parentGroupMembership.get("user").get("id"), user.getId()));
+            return cb.or(cb.notEqual(root.get("type"), EventType.VOTE),
+                    cb.greaterThan(root.get("createdDateTime"), parentGroupMembership.get("joinTime")));
+        };
+
+        return Specifications.where(basicProperties)
+                .and(EventSpecifications.hasAllUsersAssignedOrIsAssigned(user))
+                .and(userInAncestorGroup)
+                .and(userJoinedAfterVote);
+    }
+
+    public static Specification<Event> hasAllUsersAssignedOrIsAssigned(User user) {
+        return (root, query, cb) ->
+                cb.or(cb.isEmpty(root.get("assignedMembers")), cb.isMember(user, root.get("assignedMembers")));
+    }
 
     public static Specification<Event> hasGroupAsAncestor(Group group) {
         return (root, query, cb) -> cb.equal(root.get(Event_.ancestorGroup), group);
