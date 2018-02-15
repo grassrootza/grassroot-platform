8df2862b25fa467b62db99a8f5dc6c6245eb428c
Luke Jordan <luke-grassroot@users.noreply.github.com>
2018-02-15 18:22:59 +0200

    Fixed some bugs, restored Izwe Lami
    * Attempting solution to CORS issue
    
    * Fixed a bug on email only member importing, and made range of tweaks to broadcasts
    
    * Range of fixes on broadcasts, added cache to social media responses
    
    * Fixes on member adding, filtering
    
    * changes made to port live wire (#266)
    
    * Range of changes for task teams, topic setting
    
    * Overhaul and improvement of isiZulu translations, step 1
    
    * Bug fix for sending some messages, version bump to align core
    
    * Nginx logs (#269)
    
    * nginx logs redirection
    
    * nginx logs redirection 2
    
    * update to gr-test
    
    * nginx log update 1
    
    * nginx log update 2
    
    * nginx log update 3
    
    * nginx log update 4
    
    * nginx log update 5
    
    * Switched deployment back to preprod
    
    * And switched env back to stg
    
    * Wip new frontends (#271)
    
    * Zulu fixes, bug fixes, some topics and task team fixes
    
    * Attempting solution to CORS issue
    
    * Fixed a bug on email only member importing, and made range of tweaks to broadcasts
    
    * Range of fixes on broadcasts, added cache to social media responses
    
    * Fixes on member adding, filtering
    
    * changes made to port live wire (#266)
    
    * Range of changes for task teams, topic setting
    
    * Overhaul and improvement of isiZulu translations, step 1
    
    * Bug fix for sending some messages, version bump to align core
    
    * Updating various language files
    
    * Switched upcoming events to specs, filtered for non-assignment, also upped core dep versions
    
    * Fixed small but breaking todo bug on USSD
    
    * Added in Izwe Lami language strings, fixed various bugs

--- grassroot-core/src/main/java/za/org/grassroot/core/util/FormatUtil.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/util/FormatUtil.java
@@ -12,8 +12,7 @@ public class FormatUtil {
     /*
     This method drop trailing zeroes and the decimal if only zeroes after the decimal, I hope
      */
-    public static String formatDoubleToString(double d)
-    {
+    public static String formatDoubleToString(double d) {
         if(d == (long) d)
             return String.format("%d",(long)d);
         else
