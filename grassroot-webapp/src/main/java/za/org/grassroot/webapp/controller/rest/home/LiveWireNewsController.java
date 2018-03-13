8d07cf35dca0de3292ce5fbd21929707d1c2d9ba
Luke Jordan <luke-grassroot@users.noreply.github.com>
2018-03-13 20:45:46 +0200

    Bug fix on public news controller
    * Some bug fixes and tweaks for campaign type handling, topics
    
    * Changes for account page. (GCI-101)
    
    * Couple bug fixes, security patches
    
    * Bug fix and tweaks to REST account methods
    
    * Search (#276)
    
    * changes on search controler
    
    * created method to find public groups and meetings
    
    * added method to send join request
    
    * Api operation value edited
    
    * Some guidance on cleaning up search controller
    
    * cleaning up search controller
    
    * Neatening up of search controller, switched to better API path construction
    
    * Bug fixes for group searching, removed expensive log
    
    * Range of bug fixes, from group creation for email only, to broadcasts
    
    * Built out end points for upcoming public home controllers, range of changes in media handling, campaigns
    
    * Added fetch cost this month for broadcast (GCI-122). Fixed bug in NotificationSpecifications groupLog was not taken in ancestorGroupIs.
    
    * Changes for group topic manager (GCI-112)
    
    * Range of fixes to campaign, most importantly, update settings, and account connection
    
    * Fixed some failing tests
    
    * Upped version numbers
    
    * Added user language to member filter. (GCI-99)
    
    * Added configurations in MVCCOnfig to allow news component to access api
    
    * removed sendTime on livewire create method

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/rest/home/LiveWireNewsController.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/rest/home/LiveWireNewsController.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +13,7 @@ import za.org.grassroot.webapp.controller.rest.Grassroot2RestController;
 
 @Slf4j @RestController
 @Grassroot2RestController
-@RequestMapping("/news")
+@RequestMapping("/api/news")
 public class LiveWireNewsController {
 
     private final LiveWireAlertBroker liveWireAlertBroker;
