48970b9efac24694ee8502241e31768c9a9103c5
March Ratsela <ratselamarch@gmail.com>
2018-03-14 16:12:55 +0200

    created a method to get LiveWireAlerts

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/rest/livewire/LiveWireController.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/controller/rest/livewire/LiveWireController.java
@@ -1,9 +1,12 @@
 package za.org.grassroot.webapp.controller.rest.livewire;
 
 import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
@@ -16,25 +19,27 @@ import za.org.grassroot.core.enums.LiveWireAlertDestType;
 import za.org.grassroot.core.enums.LiveWireAlertType;
 import za.org.grassroot.core.enums.LocationSource;
 import za.org.grassroot.core.enums.UserInterfaceType;
+import za.org.grassroot.integration.messaging.JwtService;
 import za.org.grassroot.integration.storage.StorageBroker;
 import za.org.grassroot.services.group.GroupBroker;
 import za.org.grassroot.services.livewire.DataSubscriberBroker;
 import za.org.grassroot.services.livewire.LiveWireAlertBroker;
 import za.org.grassroot.services.task.EventBroker;
 import za.org.grassroot.services.user.UserManagementService;
+import za.org.grassroot.webapp.controller.rest.BaseRestController;
 import za.org.grassroot.webapp.enums.RestMessage;
+import za.org.grassroot.webapp.model.LiveWireAlertDTO;
 import za.org.grassroot.webapp.model.rest.wrappers.ResponseWrapper;
 import za.org.grassroot.webapp.util.RestUtil;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.Instant;
 import java.util.Set;
 
-@RestController
+@RestController @Slf4j
 @Api("/api/livewire")
 @RequestMapping(value = "/api/livewire")
-public class LiveWireController {
-
-    private static final Logger logger = LoggerFactory.getLogger(LiveWireController.class);
+public class LiveWireController extends BaseRestController{
 
     private final UserManagementService userManagementService;
     private final GroupBroker groupBroker;
@@ -45,7 +50,14 @@ public class LiveWireController {
     private final DataSubscriberBroker subscriberBroker;
 
     @Autowired
-    public LiveWireController(UserManagementService userManagementService, GroupBroker groupBroker, EventBroker eventBroker, StorageBroker storageBroker, LiveWireAlertBroker liveWireAlertBroker, DataSubscriberBroker subscriberBroker) {
+    public LiveWireController(UserManagementService userManagementService,
+                              GroupBroker groupBroker,
+                              EventBroker eventBroker,
+                              StorageBroker storageBroker,
+                              LiveWireAlertBroker liveWireAlertBroker,
+                              DataSubscriberBroker subscriberBroker,
+                              JwtService jwtService) {
+        super(jwtService,userManagementService);
         this.userManagementService = userManagementService;
         this.groupBroker = groupBroker;
         this.eventBroker = eventBroker;
@@ -80,7 +92,7 @@ public class LiveWireController {
                 .contactNumber(contactNumber)
                 .type(type);
 
-        logger.info("do we have mediaFiles? {}", mediaFileKeys);
+        log.info("do we have mediaFiles? {}", mediaFileKeys);
 
         if (LiveWireAlertType.INSTANT.equals(type)) {
             builder.group(groupBroker.load(groupUid));
@@ -108,5 +120,12 @@ public class LiveWireController {
 
         return RestUtil.okayResponseWithData(RestMessage.LIVEWIRE_ALERT_CREATED,
                 liveWireAlertBroker.createAsComplete(userUid, builder));
+    }
+
+    @RequestMapping(value = "/list",method = RequestMethod.GET)
+    public Page<LiveWireAlertDTO> getLiveWireAlerts(HttpServletRequest request,
+                                                    Pageable pageable){
+        log.info("User uid.......................... {}",getUserIdFromRequest(request));
+        return liveWireAlertBroker.loadAlerts(getUserIdFromRequest(request),false,pageable).map(LiveWireAlertDTO::new);
     }
 }
