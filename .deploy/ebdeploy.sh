d2d248f2d1175f02f979669e3f3a317d60392d3c
Luke Jordan <luke.jordan@grassroot.org.za>
2018-06-17 16:11:30 +0200

    Fixed flyers in wrong script place

--- .deploy/ebdeploy.sh
+++ .deploy/ebdeploy.sh
@@ -23,6 +23,9 @@ aws s3 cp s3://$S3BUCKET/grassroot-integration.properties.$ENVIRONMENT environme
 aws s3 cp s3://$S3BUCKET/grassroot-payments.properties.$ENVIRONMENT environment/grassroot-payments.properties --region $S3REGION
 aws s3 cp s3://$S3BUCKET/jwt_keystore.jks.$ENVIRONMENT environment/jwt_keystore.jks --region $S3REGION
 
+# DOWNLOAD PDF TEMPLATES (AT SOME POINT JUST FETCH FROM S3 DIRECTLY IN APP)
+aws s3 cp s3://$S3BUCKET/pdf_templates/ templates/pdf/ --region $S3REGION --recursive
+
 # STORE DEPLOYMENT DETAILS FOR FURTHER DEBUG
 echo $COMMIT_MESSAGE > deploy_status.txt
 
