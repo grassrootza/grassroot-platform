d9f9c77f935b6fd8743b9bc613939a90e422f850
Luke Jordan <luke-grassroot@users.noreply.github.com>
2018-06-17 13:11:59 +0200

    Circle 2 may be done, possibly (#346)
    * Accelerating Circle2 migration
    
    * Removed prod job for now
    
    * Brought across download deps
    
    * Extending into deploying
    
    * Switching over pip
    
    * Ugh
    
    * Handling CI2 env nonsense
    
    * Again with env setting
    
    * Purified Docker file
    
    * Ugh, Docker login
    
    * Ugh, Docker login redux
    
    * Now EB and AWS CLI
    
    * AWS CLI path
    
    * Brought in EB CLI
    
    * Inserted some shell prints for debugging
    
    * Little more for debugging
    
    * Weird EB issue
    
    * Weird EB issue redux
    
    * Again with EB CLI
    
    * Again with EB CLI 2
    
    * Fixed deep EB deploy issue, maybe
    
    * Removed C1 config
    
    * FInalized transition, restored all tasks
    
    * Urgh
    
    * Tweaking, maybe close
    
    * Various further fixes, trying to escape hell
    
    * Moved Supervisord to right place
    
    * Finalizing, hopefully maybe

--- .deploy/buildimage.sh
+++ .deploy/buildimage.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+# Check this
+echo "environment: $ENVIRONMENT"
+
+# Define
+S3BUCKET=grassroot-circleci
+S3REGION=eu-west-1
+
 # GET THE COMMIT ID TO SET AS PART OF THE INSTANCE NAME
 COMMITID=$(git rev-parse --short HEAD)
 sed -i "s/<BUILDID>/$COMMITID/" .deploy/startgrassroot.sh.$ENVIRONMENT
@@ -15,7 +22,20 @@ mv .deploy/log_files.yml log_files.yml
 chmod +x build-jar.sh
 chmod +x startgrassroot.sh
 chmod +x stopgrassroot.sh
+
+# DOWNLOAD ENVIRONMENT VARIABLES AND CREDENTIALS FROM S3
+aws s3 cp s3://$S3BUCKET/environment-variables.$ENVIRONMENT environment/environment-variables --region $S3REGION
+aws s3 cp s3://$S3BUCKET/aws-credentials.$ENVIRONMENT environment/aws-credentials --region $S3REGION
+aws s3 cp s3://$S3BUCKET/grassroot-integration.properties.$ENVIRONMENT environment/grassroot-integration.properties --region $S3REGION
+aws s3 cp s3://$S3BUCKET/grassroot-payments.properties.$ENVIRONMENT environment/grassroot-payments.properties --region $S3REGION
+aws s3 cp s3://$S3BUCKET/jwt_keystore.jks.$ENVIRONMENT environment/jwt_keystore.jks --region $S3REGION
+
+# DOWNLOAD PDF TEMPLATES (AT SOME POINT JUST FETCH FROM S3 DIRECTLY IN APP)
+aws s3 cp s3://$S3BUCKET/pdf_templates/ templates/pdf/ --region $S3REGION --recursive
+
+echo "Finished downloading, proceeding to build docker image"
+
 #docker build --rm=false -t awsassembly/grassroot:$ENVIRONMENT$SHA1 .
 docker build --rm=false -t grassrootdocker/gr-app:$ENVIRONMENT .
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push grassrootdocker/gr-app:$ENVIRONMENT
--- .deploy/ebdeploy.sh
+++ .deploy/ebdeploy.sh
@@ -5,8 +5,6 @@
 SHA1=$1
 ENVIRONMENT=$2
 EBENVIRONMENT=$3
-S3BUCKET=grassroot-circleci
-S3REGION=eu-west-1
 
 # GET GIT LATEST COMMIT COMMENT TO SET AS APP VERSION FOR BEANSTALK #
 COMMIT_MESSAGE=$(git rev-parse --short HEAD && date +"%m-%d-%Y-%H-%M")
@@ -16,16 +14,6 @@ export COMMIT_MESSAGE
 DOCKERRUN_FILE=Dockerrun.aws.json
 sed -i "s/<TAG>/$ENVIRONMENT/" .deploy/$DOCKERRUN_FILE
 
-# DOWNLOAD ENVIRONMENT VARIABLES AND CREDENTIALS FROM S3
-aws s3 cp s3://$S3BUCKET/environment-variables.$ENVIRONMENT environment/environment-variables --region $S3REGION
-aws s3 cp s3://$S3BUCKET/aws-credentials.$ENVIRONMENT environment/aws-credentials --region $S3REGION
-aws s3 cp s3://$S3BUCKET/grassroot-integration.properties.$ENVIRONMENT environment/grassroot-integration.properties --region $S3REGION
-aws s3 cp s3://$S3BUCKET/grassroot-payments.properties.$ENVIRONMENT environment/grassroot-payments.properties --region $S3REGION
-aws s3 cp s3://$S3BUCKET/jwt_keystore.jks.$ENVIRONMENT environment/jwt_keystore.jks --region $S3REGION
-
-# DOWNLOAD PDF TEMPLATES (AT SOME POINT JUST FETCH FROM S3 DIRECTLY IN APP)
-aws s3 cp s3://$S3BUCKET/pdf_templates/ templates/pdf/ --region $S3REGION --recursive
-
 # STORE DEPLOYMENT DETAILS FOR FURTHER DEBUG
 echo $COMMIT_MESSAGE > deploy_status.txt
 
@@ -34,7 +22,6 @@ mv .deploy/.elasticbeanstalk .elasticbeanstalk
 mv .deploy/.ebextensions .ebextensions
 mv .deploy/.ebignore .ebignore
 mv .deploy/Dockerrun.aws.json Dockerrun.aws.json
-mv .deploy/log_files.yml log_files.yml
 
 # thats just a "virtual" commit since we do not send anything back to the repository, but this is requred for beasntalke to get the latest staged version to be deployed
 git config --global user.email "grassroot@grassroot.com"
@@ -42,6 +29,10 @@ git config --global user.name "GRASSROOT"
 git add .
 git commit -m "$ENVIRONMENT-$COMMIT_MESSAGE"
 
+echo "Finished with Git, proceeding to deploy to $EBENVIRONMENT, with message $ENVIRONMENT-$COMMIT_MESSAGE"
+
 # DEPLOY APP
-eb use $EBENVIRONMENT
+eb use $EBENVIRONMENT --verbose
 eb deploy $EBENVIRONMENT --label "$ENVIRONMENT-$COMMIT_MESSAGE" --timeout 20
+
+echo "Deployment completed"
