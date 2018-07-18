f7b70d52764f269efe93d30943be42f30ddd9f2c
Luke Jordan <luke.jordan@grassroot.org.za>
2018-07-18 09:41:30 +0200

    Added further, header logging, to trace IP issue

--- grassroot-webapp/src/main/java/za/org/grassroot/webapp/interceptor/SimpleLoggingInterceptor.java
+++ grassroot-webapp/src/main/java/za/org/grassroot/webapp/interceptor/SimpleLoggingInterceptor.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * Created by paballo on 2016/03/17.
@@ -16,16 +18,17 @@ public class SimpleLoggingInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         log.info("Handling request, IP appears as: {}", request.getRemoteAddr());
+        List<String> headers = Collections.list(request.getHeaderNames());
+        Map<String, String> allHeaders = headers.stream().collect(Collectors.toMap(name -> name, request::getHeader));
+        log.info("All headers: {}", allHeaders);
         long startTime = System.currentTimeMillis();
         request.setAttribute("startTime", startTime);
         return true;
     }
 
     @Override
-    public void postHandle(
-            HttpServletRequest request, HttpServletResponse response,
-            Object handler, ModelAndView modelAndView)
-            throws Exception {
+    public void postHandle(HttpServletRequest request, HttpServletResponse response,
+            Object handler, ModelAndView modelAndView) {
 
         response.addHeader("X-Grassroot-Logged", "true");
 
