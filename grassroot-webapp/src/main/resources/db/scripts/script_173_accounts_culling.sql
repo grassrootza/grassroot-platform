c27bbe69f388ba6d3d2bbdd9b109611169d25e03
Luke Jordan <luke.jordan@grassroot.org.za>
2018-08-06 22:08:17 +0200

    Bringing around me controller up to date, also putting in place account culling

---
+++ grassroot-webapp/src/main/resources/db/scripts/script_173_accounts_culling.sql
@@ -0,0 +1,16 @@
+drop table account_billing_record;
+drop table paid_group;
+drop table acc_sponsor_request;
+
+alter table paid_account drop column last_billing_date;
+alter table paid_account drop column enabled_date_time;
+alter table paid_account drop column enabled_by_user;
+alter table paid_account drop column max_group_number;
+alter table paid_account drop column max_group_size;
+alter table paid_account drop column max_sub_group_depth;
+alter table paid_account drop column additional_reminders;
+alter table paid_account drop column last_payment_date;
+alter table paid_account drop column next_billing_date;
+alter table paid_account drop column billing_user;
+alter table paid_account drop column todos_per_month;
+alter table paid_account drop column events_per_month;
