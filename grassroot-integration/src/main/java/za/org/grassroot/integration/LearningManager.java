c920cce16ee46946f5b9d690b9d6504e0253f2ea
Luke Jordan <luke-grassroot@users.noreply.github.com>
2018-11-15 16:40:52 +0200

    Switching NLU to new lambda and Duckling container design, further simplication (#389)

--- grassroot-integration/src/main/java/za/org/grassroot/integration/LearningManager.java
+++ grassroot-integration/src/main/java/za/org/grassroot/integration/LearningManager.java
@@ -2,22 +2,15 @@ package za.org.grassroot.integration;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpStatusCodeException;
-import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import za.org.grassroot.integration.exception.SeloApiCallFailure;
 import za.org.grassroot.integration.exception.SeloParseDateTimeFailure;
 
-import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by shakka on 8/15/16.
@@ -25,50 +18,17 @@ import java.util.Map;
 @Service @Slf4j
 public class LearningManager implements LearningService {
 
+    @Value("${grassroot.datetime.lambda.url:http://localhost:5000/parse}")
+    private String learningLambdaUrl;
+
     private static final String ERROR_PARSING = "ERROR_PARSING";
-    private static final String POLLING_STRING = "Tomorrow at 9am";
-    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm"); //01-11-2018T00:00
+    private static final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss"); //01-11-2018T00:00
 
     private RestTemplate restTemplate;
-    private Environment environment;
-
-    private String learningHost;
-    private int learningPort;
-    private String dateTimePath;
-    private String dateTimeParam;
-    private String relatedTermPath;
-    private String relatedTermParam;
-    private boolean pollLearning;
 
     @Autowired
-    public LearningManager(RestTemplate restTemplate, Environment environment) {
+    public LearningManager(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
-        this.environment = environment;
-    }
-
-    @PostConstruct
-    public void init() {
-        learningHost = environment.getProperty("grassroot.learning.host", "learning.grassroot.cloud"); // default to localhost if not set
-        learningPort = environment.getProperty("grassroot.learning.port", Integer.class, 80);
-        dateTimePath = environment.getProperty("grassroot.learning.datetime.path", "datetime");
-        dateTimeParam = environment.getProperty("grassroot.learning.datetime.param", "date_string");
-        relatedTermPath = environment.getProperty("grassroot.learning.relatedterm.path", "distance");
-        relatedTermParam = environment.getProperty("grassroot.learning.relatedterm.param", "text");
-        pollLearning = environment.getProperty("grassroot.learning.poll", Boolean.class, false);
-    }
-
-    @Scheduled(cron = "0 0/5 5-19 * * ?")
-    public void keepAliveParser() {
-        if (pollLearning) {
-            log.info("Keeping warm date time parser ... ");
-            try {
-                String pollingResult = restTemplate.getForObject(UriComponentsBuilder.newInstance().scheme("http").host(learningHost).port(learningPort)
-                        .path(dateTimePath).queryParam(dateTimeParam, POLLING_STRING).build().toUri(), String.class);
-                log.info("Polling date time returned: {}", pollingResult);
-            } catch (RestClientException e) {
-                log.error("Keep alive error: {}", e.getMessage());
-            }
-        }
     }
 
     @Override
@@ -79,12 +39,8 @@ public class LearningManager implements LearningService {
         try {
             // note : the rest template is autowired to use a default character encoding (UTF 8), so putting encode
             // here will double encode and throw errors, hence leave it out
-            String url = UriComponentsBuilder.newInstance()
-                    .scheme("http")
-                    .host(learningHost)
-                    .port(learningPort)
-                    .path(dateTimePath)
-                    .queryParam(dateTimeParam, phrase)
+            String url = UriComponentsBuilder.fromHttpUrl(learningLambdaUrl)
+                    .queryParam("date_string", phrase)
                     .build()
                     .toUriString();
 
@@ -106,32 +62,6 @@ public class LearningManager implements LearningService {
             // throw an error because this shouldn't happen (might be because of an error reaching the server ...)
             log.error("Error calling Selo! Error message: {}", e.toString());
             throw new SeloApiCallFailure();
-        }
-    }
-
-    @Override
-    public Map<String, Double> findRelatedTerms(String searchTerm) {
-        try {
-            String url = UriComponentsBuilder.newInstance()
-                    .scheme("http")
-                    .host(learningHost)
-                    .port(learningPort)
-                    .path(relatedTermPath)
-                    .queryParam(relatedTermParam, searchTerm)
-                    .build()
-                    .toString();
-
-            log.info("Calling learning service, with URL: {}", url);
-
-            @SuppressWarnings("unchecked")
-            Map<String, Double> returnedTerms = restTemplate.getForObject(url, HashMap.class);
-            return returnedTerms == null ? new HashMap<>() : returnedTerms;
-        } catch (ResourceAccessException|HttpStatusCodeException e) {
-            log.warn("Error calling learning service! Error: " + e.toString());
-            return new HashMap<>();
-        } catch (RestClientException e) {
-            log.error("Need to fix up the content headers", e);
-            return new HashMap<>();
         }
     }
 
