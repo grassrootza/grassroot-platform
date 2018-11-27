36f009f8687648430806701fc74f64ad68767aa2
luke-grassroot <luke.jordan@grassroot.org.za>
2018-11-27 16:55:49 +0200

    Fixed missing location log recording in address after town

--- grassroot-core/src/main/java/za/org/grassroot/core/domain/geo/UserLocationLog.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/domain/geo/UserLocationLog.java
@@ -37,11 +37,6 @@ public class UserLocationLog {
 		// for JPA
 	}
 
-	public UserLocationLog(String uid,double latitude,double longitude){
-	    this.uid = uid;
-	    this.location = new GeoLocation(latitude,longitude);
-    }
-
 	public UserLocationLog(Instant timestamp, String userUid, GeoLocation location, LocationSource locationSource) {
 		this.uid = UUID.randomUUID().toString();
 		this.timestamp = Objects.requireNonNull(timestamp);
