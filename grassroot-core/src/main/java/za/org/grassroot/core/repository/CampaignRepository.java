62b0213b5f903ee8235ac14fce0e63ccbb03311e
akani <akanim@gmail.com>
2021-01-24 03:27:46 +0200

    more efficient campaigns managed by user call

--- grassroot-core/src/main/java/za/org/grassroot/core/repository/CampaignRepository.java
+++ grassroot-core/src/main/java/za/org/grassroot/core/repository/CampaignRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import za.org.grassroot.core.domain.campaign.Campaign;
 
 import java.time.Instant;
@@ -34,5 +35,15 @@ public interface CampaignRepository extends JpaRepository<Campaign, Long>, JpaSp
     @Query(value = "select c.* from campaign c where " +
             "(to_tsvector('english', c.name) @@ to_tsquery('english', ?1))", nativeQuery = true)
     List<Campaign> findCampaignsWithNamesIncluding(String tsQuery);
+
+    @Query(value = "SELECT c.* FROM campaign c " +
+            "WHERE c.created_by_user = :userId " +
+            "OR c.ancestor_group_id IN " +
+            "   (" +
+            "       SELECT m.group_id FROM group_user_membership m " +
+            "       WHERE m.user_id = :userId AND m.role = 'ROLE_GROUP_ORGANIZER'" +
+            "   ) " +
+            "ORDER BY c.created_date_time ASC", nativeQuery = true)
+    List<Campaign> findCampaignsManagedByUser(@Param(value = "userId") Long userId);
 
 }
